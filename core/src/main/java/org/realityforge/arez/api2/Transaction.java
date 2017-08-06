713777908770f9b210aa1f5cea9c26498916f07b
Peter Donald <peter@realityforge.org>
2017-08-06 15:36:00 +1000

    Add support for nested transactions and only collect pending passivations for top level transaction

--- core/src/main/java/org/realityforge/arez/api2/Transaction.java
+++ core/src/main/java/org/realityforge/arez/api2/Transaction.java
@@ -3,6 +3,7 @@ package org.realityforge.arez.api2;
 import java.util.ArrayList;
 import java.util.Objects;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public final class Transaction
   extends ArezElement
@@ -11,29 +12,48 @@ public final class Transaction
    * List of observables that reached zero observers within the scope of the transaction.
    * When the transaction completes, these observers are passivated
    */
-  private final ArrayList<Observable> _pendingPassivations = new ArrayList<>();
+  @Nullable
+  private ArrayList<Observable> _pendingPassivations;
+  /**
+   * Reference to the transaction that was active when this transaction began. When this
+   * transaction commits, the previous transaction will be restored.
+   */
+  @Nullable
+  private final Transaction _previous;
 
-  void commit()
+  Transaction( @Nullable final Transaction previous, @Nonnull final String name )
   {
-    Guards.invariant( () -> 0 == _batchDepth,
-                      () -> String.format( "Invoked commit on transaction named '%s' when batch is active.",
-                                           _name ) );
-    passivatePendingPassivations();
+    super( name );
+    _previous = previous;
+  }
+
+  final void commit()
+  {
+    if ( isRootTransaction() )
+    {
+      //If you are the root transaction
+      passivatePendingPassivations();
+      //TODO: schedule notifications here
+    }
   }
 
   private void passivatePendingPassivations()
   {
-    //WARNING: Passivations can be enqueued during the passivation process
-    // so always need to call _pendingPassivations.size() through each iteration
-    // of loop to ensure new passivations are collected.
-    //noinspection ForLoopReplaceableByForEach
-    for( int i = 0; i < _pendingPassivations.size(); i++ )
+    assert isRootTransaction();
+    if ( null != _pendingPassivations )
     {
-      final Observable observable = _pendingPassivations.get( i );
-      observable.resetPendingPassivation();
-      if( !observable.hasObservers() )
+      //WARNING: Passivations can be enqueued during the passivation process
+      // so always need to call _pendingPassivations.size() through each iteration
+      // of loop to ensure new passivations are collected.
+      //noinspection ForLoopReplaceableByForEach
+      for ( int i = 0; i < _pendingPassivations.size(); i++ )
       {
-        observable.passivate();
+        final Observable observable = _pendingPassivations.get( i );
+        observable.resetPendingPassivation();
+        if ( !observable.hasObservers() )
+        {
+          observable.passivate();
+        }
       }
     }
   }
@@ -43,13 +63,44 @@ public final class Transaction
     Guards.invariant( observable::canPassivate,
                       () -> String.format(
                         "Invoked queueForPassivation on transaction named '%s' for observable named '%s' when observable can not be passivated.",
-                        _name,
+                        getName(),
                         observable.getName() ) );
-    Guards.invariant( () -> !_pendingPassivations.contains( observable ),
-                      () -> String.format(
-                        "Invoked queueForPassivation on transaction named '%s' for observable named '%s' when pending passivation already exists for observable.",
-                        _name,
-                        observable.getName() ) );
+    if ( null == _pendingPassivations )
+    {
+      final Transaction rootTransaction = getRootTransaction();
+      if ( null == rootTransaction._pendingPassivations )
+      {
+        rootTransaction._pendingPassivations = new ArrayList<>();
+      }
+      _pendingPassivations = rootTransaction._pendingPassivations;
+    }
+    else
+    {
+      Guards.invariant( () -> !_pendingPassivations.contains( observable ),
+                        () -> String.format(
+                          "Invoked queueForPassivation on transaction named '%s' for observable named '%s' when pending passivation already exists for observable.",
+                          getName(),
+                          observable.getName() ) );
+    }
     _pendingPassivations.add( Objects.requireNonNull( observable ) );
+  }
+
+  private boolean isRootTransaction()
+  {
+    return null == _previous;
+  }
+
+  @Nonnull
+  private Transaction getRootTransaction()
+  {
+    if ( isRootTransaction() )
+    {
+      return this;
+    }
+    else
+    {
+      assert null != _previous;
+      return _previous.getRootTransaction();
+    }
   }
 }
