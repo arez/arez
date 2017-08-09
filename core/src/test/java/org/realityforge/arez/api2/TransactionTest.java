471513521cc5b6e40cf26369e919d15bd078d3e9
Peter Donald <peter@realityforge.org>
2017-08-10 07:13:28 +1000

    Add some additional tests around transaction names

--- core/src/test/java/org/realityforge/arez/api2/TransactionTest.java
+++ core/src/test/java/org/realityforge/arez/api2/TransactionTest.java
@@ -74,4 +74,30 @@ public class TransactionTest
 
     assertFalse( context.isTransactionActive() );
   }
+
+  @Test
+  public void transactionAcceptsNullNameWhen_namesDisabled()
+    throws Exception
+  {
+    final ArezConfig.DynamicProvider provider = (ArezConfig.DynamicProvider) ArezConfig.getProvider();
+    provider.setEnableNames( false );
+
+    final ArezContext context = new ArezContext();
+
+    context.transaction( null, null, () -> assertThrows( () -> context.getTransaction().getName() ) );
+  }
+
+  @Test
+  public void transactionAcceptsNoNameWhen_namesDisabled()
+    throws Exception
+  {
+    final ArezConfig.DynamicProvider provider = (ArezConfig.DynamicProvider) ArezConfig.getProvider();
+    provider.setEnableNames( false );
+
+    final ArezContext context = new ArezContext();
+
+    assertThrows( () -> context.transaction( ValueUtil.randomString(), null, () -> {
+      assertEquals( context.getTransaction().getName(), null );
+    } ) );
+  }
 }
