80f2be5308d38a57f6ab06c57f799deb0ef86db9
Peter Donald <peter@realityforge.org>
2017-08-24 09:38:14 +1000

    Start to add tests for tracking

--- core/src/test/java/org/realityforge/arez/api2/TransactionTest.java
+++ core/src/test/java/org/realityforge/arez/api2/TransactionTest.java
@@ -1,5 +1,6 @@
 package org.realityforge.arez.api2;
 
+import java.util.ArrayList;
 import org.realityforge.guiceyloops.shared.ValueUtil;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
@@ -99,5 +100,64 @@ public class TransactionTest
     assertThrows( () -> context.transaction( ValueUtil.randomString(), null, () -> {
       assertEquals( context.getTransaction().getName(), null );
     } ) );
+  }
+
+  @Test
+  public void completeTracking()
+    throws Exception
+  {
+    final ArezContext context = new ArezContext();
+
+    assertFalse( context.isTransactionActive() );
+    assertThrows( context::getTransaction );
+
+    final String name = ValueUtil.randomString();
+    final Observer tracker = new Observer( context, ValueUtil.randomString() );
+
+    final ArrayList<Observable> dependencies = tracker.getDependencies();
+    assertEquals( dependencies.size(), 0 );
+
+    final Observable observable = new TestObservable( context, ValueUtil.randomString() );
+    assertEquals( observable.getObservers().size(), 0 );
+
+    assertEquals( tracker.getState(), ObserverState.NOT_TRACKING );
+
+    context.transaction( name, tracker, () -> {
+      assertEquals( tracker.getState(), ObserverState.UP_TO_DATE );
+      // The dependencies reference is only updated on completion
+      assertTrue( dependencies == tracker.getDependencies() );
+
+      assertEquals( observable.getObservers().size(), 0,
+                    "Ensure observers are added on completion and not immediately" );
+      assertNotEquals( context.getTransaction().getId(),
+                       observable.getLastTrackerTransactionId(),
+                       "LastTrackerTransactionId should be updated immediately" );
+
+      observable.reportObserved();
+
+      assertEquals( observable.getObservers().size(), 0 );
+      assertEquals( context.getTransaction().getId(), observable.getLastTrackerTransactionId() );
+
+      // Another observation should not change state as already observed
+      observable.reportObserved();
+
+      assertEquals( observable.getObservers().size(), 0 );
+      assertEquals( context.getTransaction().getId(), observable.getLastTrackerTransactionId() );
+
+      // The dependencies reference is only updated on completion
+      assertTrue( dependencies == tracker.getDependencies() );
+    } );
+
+    final ArrayList<Observable> postDependencies = tracker.getDependencies();
+
+    // The dependencies reference should be swapped out by now
+    assertFalse( dependencies == postDependencies );
+
+    // A single dependency that matches the observable
+    assertEquals( postDependencies.size(), 1 );
+    assertTrue( postDependencies.contains( observable ) );
+
+    // Ensure that the observable has observers updated
+    assertEquals( observable.getObservers().size(), 1 );
   }
 }
