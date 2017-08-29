b43b3c8147b4a0481072cf961dfe0ef770c5c5a9
Peter Donald <peter@realityforge.org>
2017-08-29 23:01:51 +1000

    Add some basic javadocs

--- core/src/main/java/org/realityforge/arez/api2/Observer.java
+++ core/src/main/java/org/realityforge/arez/api2/Observer.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+/**
+ * A node within Arez that is notified of changes in 0 or more Observables.
+ */
 public class Observer
   extends Node
 {
@@ -16,9 +19,12 @@ public class Observer
   @Nonnull
   private ObserverState _state = ObserverState.NOT_TRACKING;
   /**
-   * The observables that this derivation is derived from.
-   * This corresponds to the list of observables that were observed whilst
-   * tracking the last derivation. This list should contain no duplicates.
+   * The observables that this observer receives notifications from.
+   * These are the dependencies within the dependency graph and will
+   * typically correspond to the observables that were accessed in last
+   * transaction that this observer was tracking.
+   *
+   * This list should contain no duplicates.
    */
   @Nonnull
   private ArrayList<Observable> _dependencies = new ArrayList<>();
@@ -28,12 +34,28 @@ public class Observer
     super( context, name );
   }
 
+  /**
+   * Return the state of the observer.
+   *
+   * @return the state of the observer.
+   */
   @Nonnull
   public final ObserverState getState()
   {
     return _state;
   }
 
+  /**
+   * Set the state of the observer.
+   *
+   * <ul>
+   * <li>If the state changes from UP_TO_DATE to STALE or POSSIBLY_STALE then call the onBecomeStale hook method.</li>
+   * <li>If the state changes to NOT_TRACKING then call the onBecomeUnobserved hook method.</li>
+   * <li>If the state changes from NOT_TRACKING then call the onBecomeObserved hook method.</li>
+   * </ul>
+   *
+   * @param state the state of the observer.
+   */
   public final void setState( @Nonnull final ObserverState state )
   {
     if ( !state.equals( _state ) )
@@ -68,12 +90,20 @@ public class Observer
   {
   }
 
+  /**
+   * Remove all dependencies, removing this observer from all dependencies in the process.
+   */
   final void clearDependencies()
   {
     getDependencies().forEach( dependency -> dependency.removeObserver( this ) );
     getDependencies().clear();
   }
 
+  /**
+   * Return the dependencies.
+   *
+   * @return the dependencies.
+   */
   @Nonnull
   final ArrayList<Observable> getDependencies()
   {
@@ -83,6 +113,8 @@ public class Observer
   /**
    * Replace the current set of dependencies with supplied dependencies.
    * This should be the only mechanism via which the dependencies are updated.
+   *
+   * @param dependencies the new set of dependencies.
    */
   final void replaceDependencies( @Nonnull final ArrayList<Observable> dependencies )
   {
@@ -92,6 +124,12 @@ public class Observer
     invariantDependenciesBackLink( "Post replaceDependencies" );
   }
 
+  /**
+   * Ensure the dependencies list contain no duplicates.
+   * Should be optimized away if invariant checking is disabled.
+   *
+   * @param context some useful debugging context used in invariant checks.
+   */
   final void invariantDependenciesUnique( @Nonnull final String context )
   {
     Guards.invariant( () -> getDependencies().size() == new HashSet<>( getDependencies() ).size(),
@@ -102,14 +140,23 @@ public class Observer
                         getDependencies().stream().map( Node::getName ).collect( Collectors.toList() ).toString() ) );
   }
 
+  /**
+   * Ensure all dependencies contain this observer in the list of observers.
+   * Should be optimized away if invariant checking is disabled.
+   *
+   * @param context some useful debugging context used in invariant checks.
+   */
   final void invariantDependenciesBackLink( @Nonnull final String context )
   {
-    getDependencies().forEach( observable ->
-                                 Guards.invariant( () -> observable.getObservers().contains( this ),
-                                                   () -> String.format(
-                                                     "%s: Observer named '%s' has dependency observer named '%s' which does not contain observer in list of observers.",
-                                                     context,
-                                                     getName(),
-                                                     observable.getName() ) ) );
+    if ( ArezConfig.checkInvariants() )
+    {
+      getDependencies().forEach( observable ->
+                                   Guards.invariant( () -> observable.getObservers().contains( this ),
+                                                     () -> String.format(
+                                                       "%s: Observer named '%s' has dependency observer named '%s' which does not contain observer in list of observers.",
+                                                       context,
+                                                       getName(),
+                                                       observable.getName() ) ) );
+    }
   }
 }
