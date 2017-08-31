db9d79d0ea96b50f5707cc823d2b82653809312b
Peter Donald <peter@realityforge.org>
2017-09-01 08:10:05 +1000

    Add the transaction mode to the reaction

--- core/src/main/java/org/realityforge/arez/api2/Reaction.java
+++ core/src/main/java/org/realityforge/arez/api2/Reaction.java
@@ -1,5 +1,6 @@
 package org.realityforge.arez.api2;
 
+import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -10,11 +11,28 @@ public class Reaction
    * Flag indicating whether this reaction has been scheduled. i.e. has a pending reaction.
    */
   private boolean _scheduled;
+  /**
+   * The transaction mode in which the action executes.
+   */
+  @Nonnull
+  private final TransactionMode _mode;
 
-  Reaction( @Nonnull final ArezContext context, @Nullable final String name )
+  Reaction( @Nonnull final ArezContext context, @Nullable final String name, @Nonnull final TransactionMode mode )
   {
     super( context, name );
     setOnStale( this::schedule );
+    _mode = Objects.requireNonNull( mode );
+  }
+
+  /**
+   * Return the transaction mode in which the action executes.
+   *
+   * @return the transaction mode in which the action executes.
+   */
+  @Nonnull
+  final TransactionMode getMode()
+  {
+    return _mode;
   }
 
   final boolean isScheduled()
