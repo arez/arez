0eecaf58f238a524d44dbfa47cf368920e08be66
Peter Donald <peter@realityforge.org>
2017-09-01 21:24:52 +1000

    Add reaction useful during testing

---
+++ core/src/test/java/org/realityforge/arez/TestReaction.java
@@ -0,0 +1,30 @@
+package org.realityforge.arez;
+
+import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import static org.testng.Assert.*;
+
+final class TestReaction
+  implements Reaction
+{
+  private final ArrayList<Observer> _observers = new ArrayList<>();
+
+  @Override
+  public void react( @Nonnull final Observer observer )
+    throws Exception
+  {
+    observer.getDependencies().forEach( Observable::reportObserved );
+    _observers.add( observer );
+  }
+
+  void assertObserver( final int index, @Nonnull final Observer observer )
+  {
+    assertTrue( _observers.size() > index );
+    assertEquals( _observers.get( index ), observer );
+  }
+
+  int getCallCount()
+  {
+    return _observers.size();
+  }
+}
