57bc2f32a46399c5da9f3bf07d46bc2191e67ff7
Peter Donald <peter@realityforge.org>
2017-09-02 21:11:54 +1000

    Add some minimal javadocs

--- core/src/main/java/org/realityforge/arez/ArezContext.java
+++ core/src/main/java/org/realityforge/arez/ArezContext.java
@@ -36,6 +36,14 @@ public final class ArezContext
   @Nonnull
   private final ObserverErrorHandlerSupport _observerErrorHandlerSupport = new ObserverErrorHandlerSupport();
 
+  /**
+   * Create an observer with specified parameters.
+   *
+   * @param name the name of the observer. Should be non null if {@link ArezConfig#enableNames()} returns true, false otherwise.
+   * @param mode the mode in which the reaction is executed if any.
+   * @param reaction the reaction defining observer.
+   * @param runImmediately true to invoke reaction immediately, false to schedule reaction for next reaction cycle.
+   */
   public Observer createObserver( @Nullable final String name,
                                   @Nonnull final TransactionMode mode,
                                   @Nullable final Reaction reaction,
