a908f60ab0fa48118bade9a28677adf5cde542fb
Peter Donald <peter@realityforge.org>
2017-09-05 10:59:31 +1000

    Add action annotation

---
+++ annotations/src/main/java/org/realityforge/arez/annotations/Action.java
@@ -0,0 +1,19 @@
+package org.realityforge.arez.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Methods marked with this annotation are invoked in an Arez transaction.
+ */
+@Retention( RetentionPolicy.RUNTIME )
+@Target( ElementType.METHOD )
+public @interface Action
+{
+  /**
+   * Does the action mutate state or not.
+   */
+  boolean mutation() default true;
+}
