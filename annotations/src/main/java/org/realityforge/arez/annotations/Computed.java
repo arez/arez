bf1bd72ab61166a594bcbdba1bcb54720a663c75
Peter Donald <peter@realityforge.org>
2017-09-06 09:47:43 +1000

    Update documentation to notify that @Computed methods should not throw exceptions

--- annotations/src/main/java/org/realityforge/arez/annotations/Computed.java
+++ annotations/src/main/java/org/realityforge/arez/annotations/Computed.java
@@ -8,10 +8,10 @@ import java.lang.annotation.Target;
 /**
  * Methods marked with this annotation are computed values within Arez.
  *
- * <p>The method should take zero parameters and return a single value. The value
- * should be derived from other Observables within the Arez system and the value
- * returned by the method should not change unless the state of the other Observables
- * changes. The method should NOT modify other state in the system.</p>
+ * <p>The method should take zero parameters, return a single value and throw no
+ * exception. The value should be derived from other Observables within the Arez system
+ * and the value returned by the method should not change unless the state of the other
+ * Observables changes. The method should NOT modify other state in the system.</p>
  *
  * <p>The computed method is automatically wrapped in a READ_ONLY transaction
  * by the annotation parser.</p>
