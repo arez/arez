1a549de1bd0236f2f34e6077748173c869e7f63e
Peter Donald <peter@realityforge.org>
2017-09-06 16:04:54 +1000

    Test compute where value changes

--- core/src/test/java/org/realityforge/arez/ComputedValueTest.java
+++ core/src/test/java/org/realityforge/arez/ComputedValueTest.java
@@ -53,4 +53,36 @@ public class ComputedValueTest
 
     assertEquals( computedValue.computeValue(), "XXX" );
   }
+
+  @Test
+  public void compute()
+    throws Exception
+  {
+    final ArezContext context = new ArezContext();
+    final String name = ValueUtil.randomString();
+    final AtomicReference<String> value = new AtomicReference<>();
+    final String value1 = ValueUtil.randomString();
+    final String value2 = ValueUtil.randomString();
+    final SafeFunction<String> function = value::get;
+    final EqualityComparator<String> comparator = Objects::equals;
+    final ComputedValue<String> computedValue = new ComputedValue<>( context, name, function, comparator );
+
+    setCurrentTransaction( context, computedValue.getObserver() );
+
+    final Observer observer = newReadOnlyObserver( context );
+    observer.setState( ObserverState.POSSIBLY_STALE );
+    computedValue.getObserver().getDerivedValue().addObserver( observer );
+    observer.getDependencies().add( computedValue.getObservable() );
+
+    computedValue.getObservable().setLeastStaleObserverState( ObserverState.POSSIBLY_STALE );
+
+    computedValue.setValue( value1 );
+
+    value.set( value2 );
+
+    computedValue.compute();
+
+    assertEquals( computedValue.getValue(), value2 );
+    assertEquals( observer.getState(), ObserverState.STALE );
+  }
 }
