772c9100a4814e7dda49a30b5aadca6117068531
Peter Donald <peter@realityforge.org>
2017-09-06 21:44:43 +1000

    Add test on get() for scenario when ComputedValue is up to date

--- core/src/test/java/org/realityforge/arez/ComputedValueTest.java
+++ core/src/test/java/org/realityforge/arez/ComputedValueTest.java
@@ -136,4 +136,23 @@ public class ComputedValueTest
     assertEquals( observer.isDisposed(), true );
     assertEquals( observer.getState(), ObserverState.INACTIVE );
   }
+
+  @Test
+  public void get_upToDateComputedValue()
+    throws Exception
+  {
+    final ArezContext context = new ArezContext();
+
+    final ComputedValue<String> computedValue =
+      new ComputedValue<>( context, ValueUtil.randomString(), () -> "", Objects::equals );
+    final Observer observer = computedValue.getObserver();
+
+    setCurrentTransaction( observer );
+
+    observer.setState( ObserverState.UP_TO_DATE );
+    computedValue.setValue( "XXX" );
+
+    assertEquals( computedValue.get(), "XXX" );
+    assertEquals( observer.getState(), ObserverState.UP_TO_DATE );
+  }
 }
