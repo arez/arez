d1ce657e28ac59d6cbc494b6f539072ea9fb5014
Peter Donald <peter@realityforge.org>
2017-09-07 08:46:58 +1000

    Verify nesting READ_WRITE tranwsaction in sZREAD_WRITE_OWNED is not allowed

--- core/src/test/java/org/realityforge/arez/ArezContextTest.java
+++ core/src/test/java/org/realityforge/arez/ArezContextTest.java
@@ -55,6 +55,25 @@ public class ArezContextTest
   }
 
   @Test
+  public void beginTransaction_attemptToNest_READ_WRITE_in_READ_WRITE_OWNED()
+    throws Exception
+  {
+    final ArezContext context = new ArezContext();
+
+    setCurrentTransaction( newDerivation( context ) );
+    final Transaction transaction = context.getTransaction();
+
+    final String name = ValueUtil.randomString();
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class,
+                    () -> context.beginTransaction( name, TransactionMode.READ_WRITE, null ) );
+    assertEquals( exception.getMessage(),
+                  "Attempting to create READ_WRITE transaction named '" + name +
+                  "' but it is nested in transaction named '" + transaction.getName() + "' with " +
+                  "mode READ_WRITE_OWNED which is not equal to READ_WRITE." );
+  }
+  @Test
   public void commitTransaction_matchingRootTransaction()
     throws Exception
   {
