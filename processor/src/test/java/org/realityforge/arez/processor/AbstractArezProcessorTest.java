03729c3cca355aaab8a6ef9a0e4f3141158f9918
Peter Donald <peter@realityforge.org>
2017-09-07 15:04:21 +1000

    Fix the generation of classnames

--- processor/src/test/java/org/realityforge/arez/processor/AbstractArezProcessorTest.java
+++ processor/src/test/java/org/realityforge/arez/processor/AbstractArezProcessorTest.java
@@ -10,8 +10,25 @@ abstract class AbstractArezProcessorTest
 {
   void assertSuccessfulCompile( @Nonnull final String classname )
   {
-    final String file = classname.replaceAll( "\\.", "/" );
-    assertSuccessfulCompile( "input/" + file + ".java", "expected/" + file + ".java" );
+    final String[] elements = classname.contains( "." ) ? classname.split( "." ) : new String[]{ classname };
+    final StringBuilder input = new StringBuilder();
+    final StringBuilder expected = new StringBuilder();
+    input.append( "input" );
+    expected.append( "expected" );
+    for ( int i = 0; i < elements.length; i++ )
+    {
+      input.append( '/' );
+      input.append( elements[ i ] );
+      expected.append( '/' );
+      if ( i == elements.length - 1 )
+      {
+        expected.append( "Arez_" );
+      }
+      expected.append( elements[ i ] );
+    }
+    input.append( ".java" );
+    expected.append( ".java" );
+    assertSuccessfulCompile( input.toString(), expected.toString() );
   }
 
   private void assertSuccessfulCompile( @Nonnull final String inputResource,
