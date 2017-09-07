5f82a964270efb74e578a44ffc0a4e66778be217
Peter Donald <peter@realityforge.org>
2017-09-07 22:37:57 +1000

    Add output from BasicObservableModel

---
+++ processor/src/test/resources/expected/Arez_BasicObservableModel.java
@@ -0,0 +1,39 @@
+import javax.annotation.Generated;
+import javax.annotation.Nonnull;
+import org.realityforge.arez.ArezContext;
+import org.realityforge.arez.Observable;
+
+@Generated( "org.realityforge.arez.processor.ArezProcessor" )
+public final class Arez_BasicObservableModel
+  extends BasicObservableModel
+{
+  @Nonnull
+  private final ArezContext $$arez$$_context;
+
+  @Nonnull
+  private final Observable $$arez$$_time;
+
+  public Arez_BasicObservableModel( @Nonnull final ArezContext $$arez$$_context )
+  {
+    super();
+    this.$$arez$$_context = $$arez$$_context;
+    this.$$arez$$_time = $$arez$$_context.createObservable( "BasicObservableModel.time" );
+  }
+
+  @Override
+  public long getTime()
+  {
+    this.$$arez$$_time.reportObserved();
+    return super.getTime();
+  }
+
+  @Override
+  public void setTime( final long time )
+  {
+    if ( time != super.getTime() )
+    {
+      super.setTime( time );
+      this.$$arez$$_time.reportObserved();
+    }
+  }
+}
