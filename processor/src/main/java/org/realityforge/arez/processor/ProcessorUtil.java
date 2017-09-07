2c9f2b09fda7f4d5dbad266a9e9fa1e0cbaa545e
Peter Donald <peter@realityforge.org>
2017-09-07 15:10:40 +1000

    Add method that collects all methods, ensuring defaults are replaced in required order

--- processor/src/main/java/org/realityforge/arez/processor/ProcessorUtil.java
+++ processor/src/main/java/org/realityforge/arez/processor/ProcessorUtil.java
@@ -3,8 +3,14 @@ package org.realityforge.arez.processor;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeVariableName;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import javax.annotation.Nonnull;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.type.TypeVariable;
@@ -31,5 +37,30 @@ final class ProcessorUtil
   {
     final List<TypeVariableName> variables = getTypeArgumentsAsNames( declaredType );
     return variables.toArray( new TypeName[ variables.size() ] );
+  }
+
+  @Nonnull
+  static List<ExecutableElement> getMethods( @Nonnull final TypeElement element )
+  {
+    final Map<String, ExecutableElement> methodMap = new LinkedHashMap<>();
+    enumerateMethods( element, methodMap );
+    return new ArrayList<>( methodMap.values() );
+  }
+
+  private static void enumerateMethods( @Nonnull final TypeElement element,
+                                        @Nonnull final Map<String, ExecutableElement> methods )
+  {
+    for ( final TypeMirror interfaceType : element.getInterfaces() )
+    {
+      final TypeElement interfaceElement = (TypeElement) ( (DeclaredType) interfaceType ).asElement();
+      enumerateMethods( interfaceElement, methods );
+    }
+    for ( final Element member : element.getEnclosedElements() )
+    {
+      if ( member.getKind() == ElementKind.METHOD )
+      {
+        methods.put( member.getSimpleName().toString(), (ExecutableElement) member );
+      }
+    }
   }
 }
