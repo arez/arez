a142c46c5aae7d80b0ac34cff3fcbc68db3c7795
Peter Donald <peter@realityforge.org>
2017-09-07 21:28:50 +1000

    Update imports

--- processor/src/main/java/org/realityforge/arez/processor/ContainerDescriptorParser.java
+++ processor/src/main/java/org/realityforge/arez/processor/ContainerDescriptorParser.java
@@ -6,6 +6,7 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
+import javax.lang.model.element.NestingKind;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeKind;
