1828417f9d536b4ee5b81211952f0578dca22309
Peter Donald <peter@realityforge.org>
2017-09-08 10:03:16 +1000

    Verify @ContainerId will generate appropriate id for observables

--- processor/src/test/java/org/realityforge/arez/processor/ArezProcessorTest.java
+++ processor/src/test/java/org/realityforge/arez/processor/ArezProcessorTest.java
@@ -13,6 +13,7 @@ public class ArezProcessorTest
   {
     return new Object[][]
       {
+        new Object[]{ "ContainerIdOnModel" },
         new Object[]{ "BasicModelWithDifferentAccessLevels" },
         new Object[]{ "ObservableWithCtorModel" },
         new Object[]{ "ObservableWithExceptionModel" },
---
+++ processor/src/test/resources/expected/Arez_ContainerIdOnModel.java
@@ -0,0 +1,45 @@
+import javax.annotation.Generated;
+import javax.annotation.Nonnull;
+import org.realityforge.arez.ArezContext;
+import org.realityforge.arez.Observable;
+
+@Generated( "org.realityforge.arez.processor.ArezProcessor" )
+public final class Arez_ContainerIdOnModel
+  extends ContainerIdOnModel
+{
+  @Nonnull
+  private final ArezContext $$arez$$_context;
+
+  @Nonnull
+  private final Observable $$arez$$_time;
+
+  public Arez_ContainerIdOnModel( @Nonnull final ArezContext $$arez$$_context )
+  {
+    super();
+    this.$$arez$$_context = $$arez$$_context;
+    this.$$arez$$_time =
+      $$arez$$_context.createObservable( this.$$arez$$_context.areNamesEnabled() ? $$arez$$_id() + "time" : null );
+  }
+
+  private String $$arez$$_id()
+  {
+    return "ContainerIdOnModel." + getId() + ".";
+  }
+
+  @Override
+  public long getTime()
+  {
+    this.$$arez$$_time.reportObserved();
+    return super.getTime();
+  }
+
+  @Override
+  public void setTime( final long time )
+  {
+    if ( time != super.getTime() )
+    {
+      super.setTime( time );
+      this.$$arez$$_time.reportObserved();
+    }
+  }
+}
