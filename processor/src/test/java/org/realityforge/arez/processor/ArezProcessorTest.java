8ad2992107fbbc541d031e9bb2afc7ccce17bcd5
Peter Donald <peter@realityforge.org>
2017-09-08 11:42:00 +1000

    Add test for basic action

--- processor/src/test/java/org/realityforge/arez/processor/ArezProcessorTest.java
+++ processor/src/test/java/org/realityforge/arez/processor/ArezProcessorTest.java
@@ -13,6 +13,7 @@ public class ArezProcessorTest
   {
     return new Object[][]
       {
+        new Object[]{ "BasicActionModel" },
         new Object[]{ "ObservableWithAnnotatedCtorModel" },
         new Object[]{ "ObservableModelWithUnconventionalNames" },
         new Object[]{ "DifferentObservableTypesModel" },
---
+++ processor/src/test/resources/input/BasicActionModel.java
@@ -0,0 +1,11 @@
+import org.realityforge.arez.annotations.Action;
+import org.realityforge.arez.annotations.Container;
+
+@Container
+public class BasicActionModel
+{
+  @Action
+  public void doStuff( final long time )
+  {
+  }
+}
