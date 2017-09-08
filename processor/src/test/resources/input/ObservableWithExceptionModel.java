a61cc5aa55d51fbf9b34969a5c3c0bf0e27dd26c
Peter Donald <peter@realityforge.org>
2017-09-08 10:11:52 +1000

    Decruft

--- processor/src/test/resources/input/ObservableWithExceptionModel.java
+++ processor/src/test/resources/input/ObservableWithExceptionModel.java
@@ -4,19 +4,16 @@ import org.realityforge.arez.annotations.Observable;
 @Container
 public class ObservableWithExceptionModel
 {
-  private long _time;
-
   @Observable
   public long getTime()
     throws Exception
   {
-    return _time;
+    return 0;
   }
 
   @Observable
   public void setTime( final long time )
     throws Exception
   {
-    _time = time;
   }
 }
