6caf6dddcdaf9e58cd0bc0d00716d520cf33b317
Peter Donald <peter@realityforge.org>
2017-09-08 10:20:43 +1000

    Whitespace

--- processor/src/test/resources/expected/Arez_OverrideNamesInModel.java
+++ processor/src/test/resources/expected/Arez_OverrideNamesInModel.java
@@ -3,8 +3,10 @@ import javax.annotation.Nonnull;
 import org.realityforge.arez.ArezContext;
 import org.realityforge.arez.Observable;
 
-@Generated("org.realityforge.arez.processor.ArezProcessor")
-public final class Arez_OverrideNamesInModel extends OverrideNamesInModel {
+@Generated( "org.realityforge.arez.processor.ArezProcessor" )
+public final class Arez_OverrideNamesInModel
+  extends OverrideNamesInModel
+{
   private static volatile long $$arez$$_nextId;
 
   private final long $$arez$$_id;
@@ -15,27 +17,33 @@ public final class Arez_OverrideNamesInModel extends OverrideNamesInModel {
   @Nonnull
   private final Observable $$arez$$_myField;
 
-  public Arez_OverrideNamesInModel(@Nonnull final ArezContext $$arez$$_context) {
+  public Arez_OverrideNamesInModel( @Nonnull final ArezContext $$arez$$_context )
+  {
     super();
     this.$$arez$$_id = $$arez$$_nextId++;
     this.$$arez$$_context = $$arez$$_context;
-    this.$$arez$$_myField = $$arez$$_context.createObservable( this.$$arez$$_context.areNamesEnabled() ? $$arez$$_id() + "myField" : null );
+    this.$$arez$$_myField =
+      $$arez$$_context.createObservable( this.$$arez$$_context.areNamesEnabled() ? $$arez$$_id() + "myField" : null );
   }
 
-  private String $$arez$$_id() {
+  private String $$arez$$_id()
+  {
     return "MyContainer." + $$arez$$_id + ".";
   }
 
   @Override
-  public long getTime() {
+  public long getTime()
+  {
     this.$$arez$$_myField.reportObserved();
     return super.getTime();
   }
 
   @Override
-  public void setTime(final long time) {
-    if ( time != super.getTime() ) {
-      super.setTime(time);
+  public void setTime( final long time )
+  {
+    if ( time != super.getTime() )
+    {
+      super.setTime( time );
       this.$$arez$$_myField.reportObserved();
     }
   }
