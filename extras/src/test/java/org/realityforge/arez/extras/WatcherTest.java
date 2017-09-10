ab91d518fd2e7f96977dd21dc04f9fa2a203f9f0
Peter Donald <peter@realityforge.org>
2017-09-10 15:16:08 +1000

    Add some tests for Watcher

---
+++ extras/src/test/java/org/realityforge/arez/extras/WatcherTest.java
@@ -0,0 +1,142 @@
+package org.realityforge.arez.extras;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.realityforge.arez.ArezContext;
+import org.realityforge.arez.Observable;
+import org.realityforge.arez.ObserverErrorHandler;
+import org.realityforge.arez.Procedure;
+import org.realityforge.arez.SafeFunction;
+import org.realityforge.guiceyloops.shared.ValueUtil;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class WatcherTest
+{
+  @Test
+  public void basicOperation()
+    throws Exception
+  {
+    final ArezContext context = new ArezContext();
+
+    final Observable observable = context.createObservable( ValueUtil.randomString() );
+
+    final AtomicBoolean result = new AtomicBoolean();
+
+    final AtomicInteger conditionRun = new AtomicInteger();
+    final AtomicInteger effectRun = new AtomicInteger();
+
+    final String name = ValueUtil.randomString();
+    final boolean mutation = true;
+    final SafeFunction<Boolean> condition = () -> {
+      conditionRun.incrementAndGet();
+      observable.reportObserved();
+      return result.get();
+    };
+    final Procedure procedure = effectRun::incrementAndGet;
+
+    new Watcher( context, name, mutation, condition, procedure );
+
+    assertEquals( conditionRun.get(), 1 );
+    assertEquals( effectRun.get(), 0 );
+
+    context.procedure( ValueUtil.randomString(), true, observable::reportChanged );
+
+    assertEquals( conditionRun.get(), 2 );
+    assertEquals( effectRun.get(), 0 );
+
+    result.set( true );
+
+    // Reschedule and run effect
+    context.procedure( ValueUtil.randomString(), true, observable::reportChanged );
+
+    assertEquals( conditionRun.get(), 3 );
+    assertEquals( effectRun.get(), 1 );
+
+    // Watcher should not be active anymore so this should do nothing
+    context.procedure( ValueUtil.randomString(), true, observable::reportChanged );
+
+    assertEquals( conditionRun.get(), 3 );
+    assertEquals( effectRun.get(), 1 );
+  }
+
+  @Test
+  public void dispose_releasesResources()
+    throws Exception
+  {
+    final ArezContext context = new ArezContext();
+
+    final Observable observable = context.createObservable( ValueUtil.randomString() );
+
+    final AtomicBoolean result = new AtomicBoolean();
+
+    final AtomicInteger conditionRun = new AtomicInteger();
+    final AtomicInteger effectRun = new AtomicInteger();
+
+    final String name = ValueUtil.randomString();
+    final boolean mutation = true;
+    final SafeFunction<Boolean> condition = () -> {
+      conditionRun.incrementAndGet();
+      observable.reportObserved();
+      return result.get();
+    };
+    final Procedure procedure = effectRun::incrementAndGet;
+
+    final Watcher watcher = new Watcher( context, name, mutation, condition, procedure );
+
+    assertEquals( conditionRun.get(), 1 );
+    assertEquals( effectRun.get(), 0 );
+
+    result.set( true );
+    watcher.dispose();
+
+    context.procedure( ValueUtil.randomString(), true, observable::reportChanged );
+
+    assertEquals( conditionRun.get(), 1 );
+    assertEquals( effectRun.get(), 0 );
+  }
+
+  @Test
+  public void conditionReadOnlyTransaction()
+    throws Exception
+  {
+    final ArezContext context = new ArezContext();
+
+    final AtomicInteger errorCount = new AtomicInteger();
+
+    final ObserverErrorHandler handler = ( observer, error, throwable ) -> errorCount.incrementAndGet();
+    context.addObserverErrorHandler( handler );
+
+    final Observable observable = context.createObservable( ValueUtil.randomString() );
+
+    final AtomicBoolean result = new AtomicBoolean();
+
+    final AtomicInteger conditionRun = new AtomicInteger();
+    final AtomicInteger effectRun = new AtomicInteger();
+
+    final String name = ValueUtil.randomString();
+    final boolean mutation = true;
+    final SafeFunction<Boolean> condition = () -> {
+      conditionRun.incrementAndGet();
+      observable.reportObserved();
+      observable.reportChanged();
+      return result.get();
+    };
+    final Procedure procedure = effectRun::incrementAndGet;
+
+    new Watcher( context, name, mutation, condition, procedure );
+
+    assertEquals( conditionRun.get(), 1 );
+    assertEquals( effectRun.get(), 0 );
+    assertEquals( errorCount.get(), 1 );
+
+    result.set( true );
+
+    // Attempt to run condition. Should produce another error and condition evaluation
+    context.procedure( ValueUtil.randomString(), true, observable::reportChanged );
+
+    assertEquals( conditionRun.get(), 2 );
+    assertEquals( effectRun.get(), 0 );
+    assertEquals( errorCount.get(), 2 );
+  }
+}
