799ebb2390495641e390acd1a077a2b7501e28e2
Peter Donald <peter@realityforge.org>
2017-09-11 14:46:09 +1000

    Update the description so docs can be inherited by implementers

--- core/src/main/java/org/realityforge/arez/Disposable.java
+++ core/src/main/java/org/realityforge/arez/Disposable.java
@@ -12,7 +12,7 @@ package org.realityforge.arez;
 public interface Disposable
 {
   /**
-   * Dispose element.
+   * Dispose element. See {@link Disposable} for a description of the implications.
    */
   void dispose();
 }
