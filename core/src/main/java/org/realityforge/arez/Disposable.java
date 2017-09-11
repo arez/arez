354359ed069d999de5c4f022d6961bbc950575bb
Peter Donald <peter@realityforge.org>
2017-09-11 14:44:40 +1000

    Add Disposable interface

---
+++ core/src/main/java/org/realityforge/arez/Disposable.java
@@ -0,0 +1,18 @@
+package org.realityforge.arez;
+
+/**
+ * Interface that allows caller to release any resources associated with element.
+ * It is safe to invoke {@link #dispose()} multiple times on a element. Dispose
+ * is considered a state modifying action and must be called either within an
+ * action where mutation is true or else it will start it's own transaction before
+ * performing dispose. Once an element is disposed, no other methods should be invoked
+ * on element.
+ */
+@FunctionalInterface
+public interface Disposable
+{
+  /**
+   * Dispose element.
+   */
+  void dispose();
+}
