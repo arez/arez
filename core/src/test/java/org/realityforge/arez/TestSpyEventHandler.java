afca23fa5498c108e70b067108d753e8e5a84bf5
Peter Donald <peter@realityforge.org>
2017-09-12 13:50:14 +1000

    Add SpyEventHandler useful during tests

---
+++ core/src/test/java/org/realityforge/arez/TestSpyEventHandler.java
@@ -0,0 +1,37 @@
+package org.realityforge.arez;
+
+import java.util.ArrayList;
+import javax.annotation.Nonnull;
+import static org.testng.Assert.*;
+
+final class TestSpyEventHandler
+  implements SpyEventHandler
+{
+  private final ArrayList<Object> _events = new ArrayList<>();
+
+  @Override
+  public void onSpyEvent( @Nonnull final Object event )
+  {
+    _events.add( event );
+  }
+
+  <T> T assertEvent( @Nonnull final Class<T> type, final int index )
+  {
+    assertTrue( eventCount() > index );
+    final Object event = _events.get( index );
+    assertTrue( type.isInstance( event ),
+                "Expected event at index " + index + " to be of type " + type + " but is " +
+                event + " which is of type " + event.getClass() );
+    return type.cast( event );
+  }
+
+  void assertEventCount( final int count )
+  {
+    assertEquals( eventCount(), count );
+  }
+
+  int eventCount()
+  {
+    return _events.size();
+  }
+}
