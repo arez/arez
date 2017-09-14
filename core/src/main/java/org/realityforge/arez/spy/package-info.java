c655a7a696d3d4517c2f731a703021ffedbe3100
Peter Donald <peter@realityforge.org>
2017-09-15 09:01:47 +1000

    Update javadocs to more accurately reflect intent

--- core/src/main/java/org/realityforge/arez/spy/package-info.java
+++ core/src/main/java/org/realityforge/arez/spy/package-info.java
@@ -1,8 +1,7 @@
 /**
- * This package contains the core spy events used in an Arez system.
- * While the {@link org.realityforge.arez} package and this package are
- * highly inter-dependent they are kept as separate packages as everything
- * within this package is typically compiled out in production builds when
- * spies are disabled.
+ * This package contains the spy events and models used in an Arez system.
+ * The {@link org.realityforge.arez} package and this package are
+ * highly inter-dependent but they are kept separated as production builds
+ * typically compile out everything in this package when spies are disabled.
  */
 package org.realityforge.arez.spy;
