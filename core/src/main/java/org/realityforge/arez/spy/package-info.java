d5b3a98d25e4bed3ef0bc405f78fe0e44ab96bfc
Peter Donald <peter@realityforge.org>
2017-09-14 14:12:54 +1000

    Document spy package

---
+++ core/src/main/java/org/realityforge/arez/spy/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains the core spy events used in an Arez system.
+ * While the {@link org.realityforge.arez} package and this package are
+ * highly inter-dependent they are kept as separate packages as everything
+ * within this package is typically compiled out in production builds when
+ * spies are disabled.
+ */
+package org.realityforge.arez.spy;
