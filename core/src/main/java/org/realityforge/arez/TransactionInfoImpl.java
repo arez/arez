6bc35f8534bacf705c879a0ac2d59a0b8d04253e
Peter Donald <peter@realityforge.org>
2017-09-16 14:32:56 +1000

    Add TransactionInfo spy interface and adapter for internal Transaction

---
+++ core/src/main/java/org/realityforge/arez/TransactionInfoImpl.java
@@ -0,0 +1,98 @@
+package org.realityforge.arez;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.realityforge.arez.spy.TransactionInfo;
+
+/**
+ * Adapter of Transaction to TransactionInfo for spy capabilities.
+ */
+final class TransactionInfoImpl
+  implements TransactionInfo
+{
+  @Nonnull
+  private final Transaction _transaction;
+  @Nullable
+  private TransactionInfo _parent;
+
+  TransactionInfoImpl( @Nonnull final Transaction transaction )
+  {
+    _transaction = Objects.requireNonNull( transaction );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nonnull
+  @Override
+  public String getName()
+  {
+    return getTransaction().getName();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isReadOnly()
+  {
+    return TransactionMode.READ_WRITE != getTransaction().getMode();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nullable
+  @Override
+  public TransactionInfo getParent()
+  {
+    if ( null != _parent )
+    {
+      return _parent;
+    }
+    else
+    {
+      final Transaction previous = getTransaction().getPrevious();
+      if ( null != previous )
+      {
+        _parent = new TransactionInfoImpl( previous );
+        return _parent;
+      }
+      else
+      {
+        return null;
+      }
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isTracking()
+  {
+    return null != getTransaction().getTracker();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nonnull
+  @Override
+  public Observer getTracker()
+  {
+    final Observer tracker = getTransaction().getTracker();
+    Guards.invariant( () -> null != tracker,
+                      () -> String.format( "Invoked getTracker on TransactionInfo named '%s' but no tracker exists.",
+                                           getName() ) );
+    assert null != tracker;
+    return tracker;
+  }
+
+  @Nonnull
+  Transaction getTransaction()
+  {
+    return _transaction;
+  }
+}
