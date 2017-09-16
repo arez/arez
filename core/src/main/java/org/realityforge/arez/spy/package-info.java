7048ba4eaecb336a9f8d5f060bc2695934417043
Peter Donald <peter@realityforge.org>
2017-09-16 13:54:11 +1000

    Mark package as unsupported to indicate that the API may change

--- core/src/main/java/org/realityforge/arez/spy/package-info.java
+++ core/src/main/java/org/realityforge/arez/spy/package-info.java
@@ -4,4 +4,7 @@
  * highly inter-dependent but they are kept separated as production builds
  * typically compile out everything in this package when spies are disabled.
  */
+@Unsupported( "This services is primary here to support DevTools and will evolve as requirements dictate" )
 package org.realityforge.arez.spy;
+
+import org.realityforge.arez.Unsupported;
