8c3c4042eb5215cb9d16b750b21e5e06c829588e
Peter Donald <peter@realityforge.org>
2017-09-18 16:26:16 +1000

    Add more tests around type parameters on actions

---
+++ processor/src/test/resources/expected/Arez_ActionTypeParametersModel.java
@@ -0,0 +1,81 @@
+import javax.annotation.Generated;
+import javax.annotation.Nonnull;
+import org.realityforge.arez.ArezContext;
+import org.realityforge.arez.Disposable;
+import org.realityforge.arez.spy.ActionCompletedEvent;
+import org.realityforge.arez.spy.ActionStartedEvent;
+
+@Generated("org.realityforge.arez.processor.ArezProcessor")
+public final class Arez_ActionTypeParametersModel<T extends Integer> extends ActionTypeParametersModel<T> implements Disposable {
+  private static volatile long $$arez$$_nextId;
+
+  private final long $$arez$$_id;
+
+  private boolean $$arez$$_disposed;
+
+  @Nonnull
+  private final ArezContext $$arez$$_context;
+
+  public Arez_ActionTypeParametersModel(@Nonnull final ArezContext $$arez$$_context) {
+    super();
+    this.$$arez$$_id = $$arez$$_nextId++;
+    this.$$arez$$_context = $$arez$$_context;
+  }
+
+  private String $$arez$$_id() {
+    return "ActionTypeParametersModel." + $$arez$$_id + ".";
+  }
+
+  @Override
+  public boolean isDisposed() {
+    return $$arez$$_disposed;
+  }
+
+  @Override
+  public void dispose() {
+    if ( !isDisposed() ) {
+      $$arez$$_disposed = true;
+    }
+  }
+
+  @Override
+  public T doStuff() {
+    assert !$$arez$$_disposed;
+    Throwable $$arez$$_throwable = null;
+    boolean $$arez$$_completed = false;
+    long $$arez$$_startedAt = 0L;
+    try {
+      if ( this.$$arez$$_context.areSpiesEnabled() && this.$$arez$$_context.getSpy().willPropagateSpyEvents() ) {
+        $$arez$$_startedAt = System.currentTimeMillis();
+        this.$$arez$$_context.getSpy().reportSpyEvent( new ActionStartedEvent( $$arez$$_id() + "doStuff", new Object[]{} ) );
+      }
+      final T $$arez$$_result = this.$$arez$$_context.safeFunction(this.$$arez$$_context.areNamesEnabled() ? $$arez$$_id() + "doStuff" : null, true, () -> super.doStuff() );
+      $$arez$$_completed = true;
+      if ( this.$$arez$$_context.areSpiesEnabled() && this.$$arez$$_context.getSpy().willPropagateSpyEvents() ) {
+        final long $$arez$$_duration = System.currentTimeMillis() - $$arez$$_startedAt;
+        this.$$arez$$_context.getSpy().reportSpyEvent( new ActionCompletedEvent( $$arez$$_id() + "doStuff", new Object[]{}, true, $$arez$$_result, $$arez$$_throwable, $$arez$$_duration ) );
+      }
+      return $$arez$$_result;
+    } catch( final RuntimeException e ) {
+      $$arez$$_throwable = e;
+      throw e;
+    } catch( final Exception e ) {
+      $$arez$$_throwable = e;
+      throw new IllegalStateException( e );
+    } catch( final Error e ) {
+      $$arez$$_throwable = e;
+      throw e;
+    } catch( final Throwable e ) {
+      $$arez$$_throwable = e;
+      throw new IllegalStateException( e );
+    } finally {
+      if ( !$$arez$$_completed ) {
+        final T $$arez$$_result = null;
+        if ( this.$$arez$$_context.areSpiesEnabled() && this.$$arez$$_context.getSpy().willPropagateSpyEvents() ) {
+          final long $$arez$$_duration = System.currentTimeMillis() - $$arez$$_startedAt;
+          this.$$arez$$_context.getSpy().reportSpyEvent( new ActionCompletedEvent( $$arez$$_id() + "doStuff", new Object[]{}, true, $$arez$$_result, $$arez$$_throwable, $$arez$$_duration ) );
+        }
+      }
+    }
+  }
+}
---
+++ processor/src/test/resources/input/ActionTypeParametersModel.java
@@ -0,0 +1,12 @@
+import org.realityforge.arez.annotations.Action;
+import org.realityforge.arez.annotations.Container;
+
+@Container
+public class ActionTypeParametersModel<T extends Integer>
+{
+  @Action
+  public T doStuff()
+  {
+    return null;
+  }
+}
