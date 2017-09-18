c53f5f779afd0e6691f5ed3f9c63d2093a1a63c4
Peter Donald <peter@realityforge.org>
2017-09-18 10:50:12 +1000

    Introduce annotations for hook methods to hook into activate/deactivate/stale states of ComputedValue

---
+++ annotations/src/main/java/org/realityforge/arez/annotations/OnDeactivate.java
@@ -0,0 +1,34 @@
+package org.realityforge.arez.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import javax.annotation.Nonnull;
+
+/**
+ * Identifies method that is called when ComputedValue changes to the INACTIVE state from any other state.
+ *
+ * <p>The method must also conform to the following constraints:</p>
+ * <ul>
+ * <li>Must not be annotated with {@link Observable}, {@link Computed}, {@link Action}, {@link OnActivate} or {@link OnStale}</li>
+ * <li>Must have 0 parameters</li>
+ * <li>Must not return a value</li>
+ * <li>Must not be private</li>
+ * <li>Must not be static</li>
+ * <li>Must not be abstract</li>
+ * <li>Must not throw exceptions</li>
+ * </ul>
+ */
+@Documented
+@Target( ElementType.METHOD )
+public @interface OnDeactivate
+{
+  /**
+   * Return the name of the ComputedValue that this method is associated with.
+   * Must be specified.
+   *
+   * @return the name of the ComputedValue.
+   */
+  @Nonnull
+  String name() default "<default>";
+}
