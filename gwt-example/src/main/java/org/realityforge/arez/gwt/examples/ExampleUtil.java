761aad54165db92db8d85405aad3edc2fc84e11f
Peter Donald <peter@realityforge.org>
2017-09-18 15:52:03 +1000

    Copy ExampleUtil so can use during GWT debugging

---
+++ gwt-example/src/main/java/org/realityforge/arez/gwt/examples/ExampleUtil.java
@@ -0,0 +1,24 @@
+package org.realityforge.arez.gwt.examples;
+
+import elemental2.dom.DomGlobal;
+import javax.annotation.Nonnull;
+import org.realityforge.arez.ArezContext;
+
+final class ExampleUtil
+{
+  private ExampleUtil()
+  {
+  }
+
+  static void logAllErrors( @Nonnull final ArezContext context )
+  {
+    context.addObserverErrorHandler( ( observer, error, throwable ) -> {
+
+      DomGlobal.console.log( "Observer error: " + error + "\nobserver: " + observer );
+      if ( null != throwable )
+      {
+        DomGlobal.console.log( throwable );
+      }
+    } );
+  }
+}
