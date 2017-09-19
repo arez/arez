75831e64d25fc832d9a54c9423fa8fcf23e549ee
Peter Donald <peter@realityforge.org>
2017-09-20 09:28:08 +1000

    Document IdleStatus

--- browser-extras/src/main/java/org/realityforge/arez/browser/extras/IdleStatus.java
+++ browser-extras/src/main/java/org/realityforge/arez/browser/extras/IdleStatus.java
@@ -4,6 +4,9 @@ import elemental2.dom.DomGlobal;
 import elemental2.dom.EventListener;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
 import org.realityforge.arez.Unsupported;
@@ -15,22 +18,80 @@ import org.realityforge.arez.annotations.OnActivate;
 import org.realityforge.arez.annotations.OnDeactivate;
 import org.realityforge.arez.annotations.PreDispose;
 
+/**
+ * Observable model declares state that tracks when the user is "idle".
+ * A user is considered idle if they have not interacted with the browser
+ * for specific amount of time.
+ *
+ * <p>Application code can observe the idle state via accessing {@link #isIdle()}.
+ * Typically this is done in a tracking transaction such as those defined by autorun.</p>
+ *
+ * <p>The "amount of time" is defined by the Observable value "timeout" accessible via
+ * {@link #getTimeout()} and mutable via {@link #setTimeout(long)}.</p>
+ *
+ * <p>The "not interacted with the browser" is detected by listening for interaction
+ * events on the browser. The list of events that the model listens for is controlled via
+ * {@link #getEvents()} and {@link #setEvents(Set)}. It should be noted that if
+ * there is no observer observing the idle state then the model will remove listeners
+ * so as not to have any significant performance impact.</p>
+ *
+ * <h1>A very simple example</h1>
+ * <pre>{@code
+ * import com.google.gwt.core.client.EntryPoint;
+ * import elemental2.dom.DomGlobal;
+ * import org.realityforge.arez.Arez;
+ * import org.realityforge.arez.browser.extras.IdleStatus;
+ *
+ * public class IdleStatusExample
+ *   implements EntryPoint
+ * {
+ *   public void onModuleLoad()
+ *   {
+ *     final IdleStatus idleStatus = IdleStatus.create();
+ *     Arez.context().autorun( () -> {
+ *       final String message = "Interaction Status: " + ( idleStatus.isIdle() ? "Idle" : "Active" );
+ *       DomGlobal.console.log( message );
+ *     } );
+ *   }
+ * }
+ * }</pre>
+ */
 @Unsupported( "This is still considered experimental and will likely evolve over time" )
 @Container( singleton = true )
 public class IdleStatus
 {
   private static final long DEFAULT_TIMEOUT = 2000L;
   private final EventListener _listener = e -> resetLastActivityTime();
-  private Collection<String> _events =
-    Arrays.asList( "keydown", "touchstart", "scroll", "mousemove", "mouseup", "mousedown", "wheel" );
+  private Set<String> _events =
+    new HashSet<>( Arrays.asList( "keydown", "touchstart", "scroll", "mousemove", "mouseup", "mousedown", "wheel" ) );
   private final DomGlobal.SetTimeoutCallbackFn _timeoutCallback = e -> onTimeout();
 
+  /**
+   * The time at which the last event was received.
+   */
   private long _lastActivityAt;
+  /**
+   * The duration for which no events should be received for the idle condition to be triggered.
+   */
   private long _timeout = DEFAULT_TIMEOUT;
+  /**
+   * True if an Observer is watching idle state.
+   */
   private boolean _active;
+  /**
+   * Short cut observable field checked after idle state is confirmed.
+   */
   private boolean _rawIdle;
+  /**
+   * The id of timeout scheduled action, 0 if none set.
+   */
   private double _timeoutId;
 
+  /**
+   * Create an instance of this model.
+   *
+   * @return an instance of IdleStatus.
+   */
   public static IdleStatus create()
   {
     return new Arez_IdleStatus();
@@ -52,6 +113,11 @@ public class IdleStatus
     cancelTimeout();
   }
 
+  /**
+   * Return true if the user is idle.
+   *
+   * @return true if the user is idle, false otherwise.
+   */
   @Computed
   public boolean isIdle()
   {
@@ -102,31 +168,57 @@ public class IdleStatus
     return _rawIdle;
   }
 
+  /**
+   * Return the timeout.
+   *
+   * @return the timeout
+   */
   @Observable
   public long getTimeout()
   {
     return _timeout;
   }
 
+  /**
+   * Set the timeout.
+   *
+   * @param timeout the timeout.
+   */
   public void setTimeout( final long timeout )
   {
     _timeout = timeout;
   }
 
+  /**
+   * Return the set of events to listen to.
+   *
+   * @return the set of events.
+   */
   @Observable
-  public Collection<String> getEvents()
+  public Set<String> getEvents()
   {
     return _events;
   }
 
-  public void setEvents( @Nonnull final Collection<String> events )
+  /**
+   * Specify the set of events to listen to.
+   * If the model is already active, the listeners will be updated to reflect the new events.
+   *
+   * @param events the set of events.
+   */
+  public void setEvents( @Nonnull final Set<String> events )
   {
-    final Collection<String> oldEvents = _events;
-    _events = Arrays.asList( events.toArray( new String[ events.size() ] ) );
+    final Set<String> oldEvents = _events;
+    final HashSet<String> newEvents = new HashSet<>();
+    newEvents.addAll( events );
+    _events = Collections.unmodifiableSet( newEvents );
     updateListeners( oldEvents, events );
   }
 
-  private void updateListeners( @Nonnull final Collection<String> oldEvents, @Nonnull final Collection<String> events )
+  /**
+   * Synchronize listeners against the dom based on new events.
+   */
+  private void updateListeners( @Nonnull final Set<String> oldEvents, @Nonnull final Set<String> events )
   {
     if ( _active )
     {
@@ -141,6 +233,11 @@ public class IdleStatus
     }
   }
 
+  /**
+   * Return the time at which the last monitored event was received.
+   *
+   * @return the time at which the last event was received.
+   */
   @Observable
   public long getLastActivityAt()
   {
