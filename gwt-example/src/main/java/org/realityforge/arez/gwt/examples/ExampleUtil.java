ad86db7003d2a3437749ac38aeec04d226459bff
Peter Donald <peter@realityforge.org>
2017-09-19 12:56:37 +1000

    Add whyRun helper

--- gwt-example/src/main/java/org/realityforge/arez/gwt/examples/ExampleUtil.java
+++ gwt-example/src/main/java/org/realityforge/arez/gwt/examples/ExampleUtil.java
@@ -2,12 +2,19 @@ package org.realityforge.arez.gwt.examples;
 
 import elemental2.dom.DomGlobal;
 import javax.annotation.Nonnull;
+import org.realityforge.arez.Arez;
 import org.realityforge.arez.ArezContext;
+import org.realityforge.arez.extras.WhyRun;
 
 final class ExampleUtil
 {
   private ExampleUtil()
   {
+  }
+
+  static void whyRun()
+  {
+    DomGlobal.console.log( new WhyRun( Arez.context().getSpy() ).whyRun() );
   }
 
   static void logAllErrors( @Nonnull final ArezContext context )
