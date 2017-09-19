71ee69f653531f5ef65897478fa0a79015d20df1
Peter Donald <peter@realityforge.org>
2017-09-19 10:15:39 +1000

    Remove unused actions

--- example/src/main/java/org/realityforge/arez/examples/TimeModel.java
+++ example/src/main/java/org/realityforge/arez/examples/TimeModel.java
@@ -1,6 +1,5 @@
 package org.realityforge.arez.examples;
 
-import java.text.ParseException;
 import javax.annotation.Nonnull;
 import org.realityforge.arez.annotations.Action;
 import org.realityforge.arez.annotations.Container;
@@ -44,25 +43,5 @@ public class TimeModel
   public void updateTime()
   {
     setTime( System.currentTimeMillis() );
-  }
-
-  @Action
-  public long calcTime()
-  {
-    return getTime();
-  }
-
-  @Action
-  public long calcTime2()
-    throws ParseException
-  {
-    return getTime();
-  }
-
-  @Action
-  public long calcTime3( int a, String b, Object c )
-    throws Exception
-  {
-    return getTime();
   }
 }
