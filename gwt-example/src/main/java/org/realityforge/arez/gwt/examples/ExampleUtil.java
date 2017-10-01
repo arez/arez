7521a4d5bd4b4bf46de5ee732b86b68e7587d0fc
Peter Donald <peter@realityforge.org>
2017-10-01 15:08:56 +1100

    Simplify the logAllErrors utility

--- gwt-example/src/main/java/org/realityforge/arez/gwt/examples/ExampleUtil.java
+++ gwt-example/src/main/java/org/realityforge/arez/gwt/examples/ExampleUtil.java
@@ -1,9 +1,7 @@
 package org.realityforge.arez.gwt.examples;
 
 import elemental2.dom.DomGlobal;
-import javax.annotation.Nonnull;
 import org.realityforge.arez.Arez;
-import org.realityforge.arez.ArezContext;
 import org.realityforge.arez.extras.WhyRun;
 
 final class ExampleUtil
@@ -23,9 +21,9 @@ final class ExampleUtil
     DomGlobal.console.log( new WhyRun( Arez.context() ).whyRun() );
   }
 
-  static void logAllErrors( @Nonnull final ArezContext context )
+  static void logAllErrors()
   {
-    context.addObserverErrorHandler( ( observer, error, throwable ) -> {
+    Arez.context().addObserverErrorHandler( ( observer, error, throwable ) -> {
       DomGlobal.console.log( "Observer error: " + error + "\nobserver: " + observer );
       if ( null != throwable )
       {
