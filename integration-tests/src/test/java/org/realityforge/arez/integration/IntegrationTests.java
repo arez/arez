8351f7e656fcd1073e39062f68537e027b080be8
Peter Donald <peter@realityforge.org>
2017-10-10 22:07:38 +1100

    Convert CodeModel example into an integration test

--- integration-tests/src/test/java/org/realityforge/arez/integration/IntegrationTests.java
+++ integration-tests/src/test/java/org/realityforge/arez/integration/IntegrationTests.java
@@ -2,6 +2,8 @@ package org.realityforge.arez.integration;
 
 import org.realityforge.arez.Arez;
 import org.realityforge.arez.ArezContext;
+import org.realityforge.arez.Disposable;
+import org.realityforge.arez.Observer;
 import org.testng.annotations.Test;
 
 public class IntegrationTests
@@ -27,6 +29,34 @@ public class IntegrationTests
 
     context.action( "First Name Update", true, () -> person.setFirstName( "Fred" ) );
     context.action( "Last Name Update", true, () -> person.setLastName( "Donaldo" ) );
+
+    assertEqualsFixture( recorder.eventsAsString() );
+  }
+
+  /**
+   * Another example that incorporates actions, computed and disposes.
+   */
+  @Test
+  public void codeModelScenario()
+    throws Throwable
+  {
+    final ArezContext context = Arez.context();
+
+    final SpyEventRecorder recorder = new SpyEventRecorder();
+    context.getSpy().addSpyEventHandler( recorder );
+
+    final CodeModel codeModel = CodeModel.create( "com.example", "MyType" );
+
+    final Observer observer =
+      context.autorun( "Printer",
+                       () -> record( recorder, "qualifiedName", codeModel.getQualifiedName() ) );
+
+    context.action( "Specific Qualified Name", true, () -> codeModel.setQualifiedName( "com.biz.Fred" ) );
+    context.action( "Reset Qualified Name to default", true, () -> codeModel.setQualifiedName( null ) );
+    context.action( "Change Local Name", true, () -> codeModel.setName( "MyType2" ) );
+
+    observer.dispose();
+    context.action( "Dispose Model", true, () -> Disposable.dispose( codeModel ) );
 
     assertEqualsFixture( recorder.eventsAsString() );
   }
