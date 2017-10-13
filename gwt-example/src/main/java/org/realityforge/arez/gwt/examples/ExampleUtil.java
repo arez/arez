d7e8ed0ec7f443518b7e29171c0d560baa2d074d
Peter Donald <peter@realityforge.org>
2017-10-14 07:55:33 +1100

    Remove code no longer required

--- gwt-example/src/main/java/org/realityforge/arez/gwt/examples/ExampleUtil.java
+++ gwt-example/src/main/java/org/realityforge/arez/gwt/examples/ExampleUtil.java
@@ -2,18 +2,12 @@ package org.realityforge.arez.gwt.examples;
 
 import elemental2.dom.DomGlobal;
 import org.realityforge.arez.Arez;
-import org.realityforge.arez.browser.extras.spy.ConsoleSpyEventProcessor;
 import org.realityforge.arez.extras.WhyRun;
 
 final class ExampleUtil
 {
   private ExampleUtil()
   {
-  }
-
-  static void spyEvents()
-  {
-    Arez.context().getSpy().addSpyEventHandler( new ConsoleSpyEventProcessor() );
   }
 
   static void jsonLogSpyEvents()
