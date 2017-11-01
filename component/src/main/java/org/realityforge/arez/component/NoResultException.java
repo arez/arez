a3dd9163e1bb05969ec04e13d1e3989590a76337
Peter Donald <peter@realityforge.org>
2017-11-01 13:21:50 +1100

    Remove constructors never used

--- component/src/main/java/org/realityforge/arez/component/NoResultException.java
+++ component/src/main/java/org/realityforge/arez/component/NoResultException.java
@@ -1,7 +1,5 @@
 package org.realityforge.arez.component;
 
-import javax.annotation.Nullable;
-
 /**
  * Exception thrown by repository when query produces no result when a result was expected.
  */
@@ -13,26 +11,5 @@ public class NoResultException
    */
   public NoResultException()
   {
-  }
-
-  /**
-   * Create the exception.
-   *
-   * @param message the detail message of exception.
-   */
-  public NoResultException( @Nullable final String message )
-  {
-    super( message );
-  }
-
-  /**
-   * Create the exception.
-   *
-   * @param message the detail message of exception.
-   * @param cause   the ultimate cause of the exception.
-   */
-  public NoResultException( @Nullable final String message, @Nullable final Throwable cause )
-  {
-    super( message, cause );
   }
 }
