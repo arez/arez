8c94661a969920fe68a32a6199d3ad9961d429b3
Peter Donald <peter@realityforge.org>
2017-11-03 09:55:24 +1100

    Start to introduce a Component abstraction in core library

---
+++ core/src/main/java/org/realityforge/arez/Component.java
@@ -0,0 +1,264 @@
+package org.realityforge.arez;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import static org.realityforge.braincheck.Guards.*;
+
+/**
+ * The component is an abstraction representation of a reactive component within Arez.
+ * Each component is made up of one or more of the core Arez reactive elements: {@link Observable}s,
+ * {@link Observer}s or {@link ComputedValue}s.
+ */
+public final class Component
+  implements Disposable
+{
+  /**
+   * Reference to the system to which this node belongs.
+   */
+  @Nonnull
+  private final ArezContext _context;
+  /**
+   * A opaque string describing the type of the component.
+   * It corresponds to @ArezComponent.name parameter if this component was built using the annotation processor.
+   */
+  @Nonnull
+  private final String _type;
+  /**
+   * The if of the component. This should be null for singletons and non-null for non-singletons.
+   */
+  @Nullable
+  private final Object _id;
+  /**
+   * A human consumable name for node. It should be non-null if {@link Arez#areNamesEnabled()} returns
+   * true and <tt>null</tt> otherwise.
+   */
+  @Nullable
+  private final String _name;
+  private final ArrayList<Observable<?>> _observables = new ArrayList<>();
+  private final ArrayList<Observer> _observers = new ArrayList<>();
+  private final ArrayList<ComputedValue<?>> _computedValues = new ArrayList<>();
+  private boolean _disposed;
+
+  Component( @Nonnull final ArezContext context,
+             @Nonnull final String type,
+             @Nullable final Object id,
+             @Nullable final String name )
+  {
+    apiInvariant( () -> Arez.areNamesEnabled() || null == name,
+                  () -> "Component passed a name '" + name + "' but Arez.areNamesEnabled() is false" );
+    _context = context;
+    _type = Objects.requireNonNull( type );
+    _id = id;
+    _name = Arez.areNamesEnabled() ? Objects.requireNonNull( name ) : null;
+  }
+
+  /**
+   * Return the component type.
+   * This is an opaque string specified by the user.
+   *
+   * @return the component type.
+   */
+  @Nonnull
+  public String getType()
+  {
+    return _type;
+  }
+
+  /**
+   * Return the unique id of the component.
+   * This will return null for singletons.
+   *
+   * @return the unique id of the component.
+   */
+  @Nullable
+  public Object getId()
+  {
+    return _id;
+  }
+
+  /**
+   * Return the unique name of the component.
+   *
+   * @return the name of the component.
+   */
+  @Nonnull
+  public String getName()
+  {
+    apiInvariant( Arez::areNamesEnabled, () -> "Component.getName() invoked when Arez.areNamesEnabled() is false" );
+    assert null != _name;
+    return _name;
+  }
+
+  @Nonnull
+  final ArezContext getContext()
+  {
+    return _context;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void dispose()
+  {
+    if ( !_disposed )
+    {
+      _disposed = true;
+      _context.safeAction( Arez.areNamesEnabled() ? getName() + ".dispose" : null, () -> {
+        _observers.forEach( o -> Disposable.dispose( o ) );
+        _observers.clear();
+        _computedValues.forEach( v -> Disposable.dispose( v ) );
+        _computedValues.clear();
+        _observables.forEach( o -> Disposable.dispose( o ) );
+        _observables.clear();
+      } );
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isDisposed()
+  {
+    return _disposed;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nonnull
+  @Override
+  public final String toString()
+  {
+    if ( Arez.areNamesEnabled() )
+    {
+      return getName();
+    }
+    else
+    {
+      return super.toString();
+    }
+  }
+
+  /**
+   * Return the observers associated with the component.
+   *
+   * @return the observers associated with the component.
+   */
+  @Nonnull
+  ArrayList<Observer> getObservers()
+  {
+    return _observers;
+  }
+
+  /**
+   * Add observer to component.
+   * Observer should not be part of observer.
+   *
+   * @param observer the observer.
+   */
+  void addObserver( @Nonnull final Observer observer )
+  {
+    apiInvariant( () -> !_observers.contains( observer ),
+                  () -> "Component.addObserver invoked on component '" + getName() + "' specifying observer " +
+                        "named '" + observer.getName() + "' when observer already exists for component." );
+    _observers.add( observer );
+  }
+
+  /**
+   * Remove observer from the component.
+   * Observer should be part of component.
+   *
+   * @param observer the observer.
+   */
+  void removeObserver( @Nonnull final Observer observer )
+  {
+    apiInvariant( () -> _observers.contains( observer ),
+                  () -> "Component.removeObserver invoked on component '" + getName() + "' specifying observer " +
+                        "named '" + observer.getName() + "' when observer does not exist for component." );
+    _observers.remove( observer );
+  }
+
+  /**
+   * Return the observables associated with the component.
+   *
+   * @return the observables associated with the component.
+   */
+  @Nonnull
+  ArrayList<Observable<?>> getObservables()
+  {
+    return _observables;
+  }
+
+  /**
+   * Add observable to component.
+   * Observable should not be part of component.
+   *
+   * @param observable the observable.
+   */
+  void addObservable( @Nonnull final Observable observable )
+  {
+    apiInvariant( () -> !_observables.contains( observable ),
+                  () -> "Component.addObservable invoked on component '" + getName() + "' specifying observable " +
+                        "named '" + observable.getName() + "' when observable already exists for component." );
+    _observables.add( observable );
+  }
+
+  /**
+   * Remove observable from the component.
+   * Observable should be part of component.
+   *
+   * @param observable the observable.
+   */
+  void removeObservable( @Nonnull final Observable observable )
+  {
+    apiInvariant( () -> _observables.contains( observable ),
+                  () -> "Component.removeObservable invoked on component '" + getName() + "' specifying observable " +
+                        "named '" + observable.getName() + "' when observable does not exist for component." );
+    _observables.remove( observable );
+  }
+
+  /**
+   * Return the computedValues associated with the component.
+   *
+   * @return the computedValues associated with the component.
+   */
+  @Nonnull
+  ArrayList<ComputedValue<?>> getComputedValues()
+  {
+    return _computedValues;
+  }
+
+  /**
+   * Add computedValue to component.
+   * ComputedValue should not be part of component.
+   *
+   * @param computedValue the computedValue.
+   */
+  void addComputedValue( @Nonnull final ComputedValue computedValue )
+  {
+    apiInvariant( () -> !_computedValues.contains( computedValue ),
+                  () -> "Component.addComputedValue invoked on component '" + getName() + "' specifying " +
+                        "computedValue named '" + computedValue.getName() + "' when computedValue already exists " +
+                        "for component." );
+    _computedValues.add( computedValue );
+  }
+
+  /**
+   * Remove computedValue from the component.
+   * ComputedValue should be part of component.
+   *
+   * @param computedValue the computedValue.
+   */
+  void removeComputedValue( @Nonnull final ComputedValue computedValue )
+  {
+    apiInvariant( () -> _computedValues.contains( computedValue ),
+                  () -> "Component.removeComputedValue invoked on component '" + getName() + "' specifying " +
+                        "computedValue named '" + computedValue.getName() + "' when computedValue does not exist " +
+                        "for component." );
+    _computedValues.remove( computedValue );
+  }
+}
---
+++ core/src/test/java/org/realityforge/arez/ComponentTest.java
@@ -0,0 +1,370 @@
+package org.realityforge.arez;
+
+import org.realityforge.guiceyloops.shared.ValueUtil;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ComponentTest
+  extends AbstractArezTest
+{
+  @Test
+  public void basicOperation()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String type = ValueUtil.randomString();
+    final String id = ValueUtil.randomString();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, type, id, name );
+    assertEquals( component.getContext(), context );
+    assertEquals( component.getType(), type );
+    assertEquals( component.getId(), id );
+    assertEquals( component.getName(), name );
+    assertEquals( component.toString(), name );
+  }
+
+  @Test
+  public void basicOperationIntegerId()
+    throws Exception
+  {
+    final Integer id = ValueUtil.randomInt();
+    final Component component = new Component( Arez.context(), ValueUtil.randomString(), id, ValueUtil.randomString() );
+    assertEquals( component.getId(), id );
+  }
+
+  @Test
+  public void basicOperationNullId()
+    throws Exception
+  {
+    final Object id = null;
+    final Component component = new Component( Arez.context(), ValueUtil.randomString(), id, ValueUtil.randomString() );
+    assertEquals( component.getId(), id );
+  }
+
+  @Test
+  public void noNameSuppliedWhenNamesDisabled()
+    throws Exception
+  {
+    ArezTestUtil.disableNames();
+
+    final ArezContext context = Arez.context();
+    final String type = ValueUtil.randomString();
+    final String id = ValueUtil.randomString();
+
+    final Component component = new Component( context, type, id, null );
+    assertEquals( component.getContext(), context );
+    assertEquals( component.getType(), type );
+    assertEquals( component.getId(), id );
+
+    assertTrue( component.toString().startsWith( "org.realityforge.arez.Component@" ) );
+
+    final IllegalStateException exception = expectThrows( IllegalStateException.class, component::getName );
+    assertEquals( exception.getMessage(), "Component.getName() invoked when Arez.areNamesEnabled() is false" );
+  }
+
+  @Test
+  public void nameSuppliedWhenNamesDisabled()
+    throws Exception
+  {
+    ArezTestUtil.disableNames();
+
+    final ArezContext context = Arez.context();
+    final String type = ValueUtil.randomString();
+    final String id = ValueUtil.randomString();
+    final String name = ValueUtil.randomString();
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> new Component( context, type, id, name ) );
+
+    assertEquals( exception.getMessage(),
+                  "Component passed a name '" + name + "' but Arez.areNamesEnabled() is false" );
+  }
+
+  @Test
+  public void observers()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final Observer observer1 = context.autorun( () -> {
+    } );
+    final Observer observer2 = context.autorun( () -> {
+    } );
+
+    assertEquals( component.getObservers().size(), 0 );
+
+    component.addObserver( observer1 );
+
+    assertEquals( component.getObservers().size(), 1 );
+    assertEquals( component.getObservers().contains( observer1 ), true );
+
+    component.addObserver( observer2 );
+
+    assertEquals( component.getObservers().size(), 2 );
+    assertEquals( component.getObservers().contains( observer1 ), true );
+    assertEquals( component.getObservers().contains( observer2 ), true );
+
+    component.removeObserver( observer1 );
+
+    assertEquals( component.getObservers().size(), 1 );
+    assertEquals( component.getObservers().contains( observer2 ), true );
+
+    component.removeObserver( observer2 );
+
+    assertEquals( component.getObservers().size(), 0 );
+  }
+
+  @Test
+  public void addObserver_duplicate()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final Observer observer1 = context.autorun( () -> {
+    } );
+
+    component.addObserver( observer1 );
+
+    assertEquals( component.getObservers().size(), 1 );
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> component.addObserver( observer1 ) );
+    assertEquals( exception.getMessage(), "Component.addObserver invoked on component '" + name +
+                                          "' specifying observer named '" + observer1.getName() +
+                                          "' when observer already exists for component." );
+
+    assertEquals( component.getObservers().size(), 1 );
+  }
+
+  @Test
+  public void removeObserver_noExist()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final Observer observer1 = context.autorun( () -> {
+    } );
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> component.removeObserver( observer1 ) );
+    assertEquals( exception.getMessage(), "Component.removeObserver invoked on component '" + name +
+                                          "' specifying observer named '" + observer1.getName() +
+                                          "' when observer does not exist for component." );
+  }
+
+  @Test
+  public void observables()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final Observable observable1 = context.createObservable( ValueUtil.randomString() );
+    final Observable observable2 = context.createObservable( ValueUtil.randomString() );
+
+    assertEquals( component.getObservables().size(), 0 );
+
+    component.addObservable( observable1 );
+
+    assertEquals( component.getObservables().size(), 1 );
+    assertEquals( component.getObservables().contains( observable1 ), true );
+
+    component.addObservable( observable2 );
+
+    assertEquals( component.getObservables().size(), 2 );
+    assertEquals( component.getObservables().contains( observable1 ), true );
+    assertEquals( component.getObservables().contains( observable2 ), true );
+
+    component.removeObservable( observable1 );
+
+    assertEquals( component.getObservables().size(), 1 );
+    assertEquals( component.getObservables().contains( observable2 ), true );
+
+    component.removeObservable( observable2 );
+
+    assertEquals( component.getObservables().size(), 0 );
+  }
+
+  @Test
+  public void addObservable_duplicate()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final Observable observable1 = context.createObservable( ValueUtil.randomString() );
+
+    component.addObservable( observable1 );
+
+    assertEquals( component.getObservables().size(), 1 );
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> component.addObservable( observable1 ) );
+    assertEquals( exception.getMessage(), "Component.addObservable invoked on component '" + name +
+                                          "' specifying observable named '" + observable1.getName() +
+                                          "' when observable already exists for component." );
+
+    assertEquals( component.getObservables().size(), 1 );
+  }
+
+  @Test
+  public void removeObservable_noExist()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final Observable observable1 = context.createObservable( ValueUtil.randomString() );
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> component.removeObservable( observable1 ) );
+    assertEquals( exception.getMessage(), "Component.removeObservable invoked on component '" + name +
+                                          "' specifying observable named '" + observable1.getName() +
+                                          "' when observable does not exist for component." );
+  }
+
+  @Test
+  public void computedValues()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final ComputedValue computedValue1 = context.createComputedValue( () -> "" );
+    final ComputedValue computedValue2 = context.createComputedValue( () -> "" );
+
+    assertEquals( component.getComputedValues().size(), 0 );
+
+    component.addComputedValue( computedValue1 );
+
+    assertEquals( component.getComputedValues().size(), 1 );
+    assertEquals( component.getComputedValues().contains( computedValue1 ), true );
+
+    component.addComputedValue( computedValue2 );
+
+    assertEquals( component.getComputedValues().size(), 2 );
+    assertEquals( component.getComputedValues().contains( computedValue1 ), true );
+    assertEquals( component.getComputedValues().contains( computedValue2 ), true );
+
+    component.removeComputedValue( computedValue1 );
+
+    assertEquals( component.getComputedValues().size(), 1 );
+    assertEquals( component.getComputedValues().contains( computedValue2 ), true );
+
+    component.removeComputedValue( computedValue2 );
+
+    assertEquals( component.getComputedValues().size(), 0 );
+  }
+
+  @Test
+  public void addComputedValue_duplicate()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final ComputedValue computedValue1 = context.createComputedValue( () -> "" );
+
+    component.addComputedValue( computedValue1 );
+
+    assertEquals( component.getComputedValues().size(), 1 );
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> component.addComputedValue( computedValue1 ) );
+    assertEquals( exception.getMessage(), "Component.addComputedValue invoked on component '" + name +
+                                          "' specifying computedValue named '" + computedValue1.getName() +
+                                          "' when computedValue already exists for component." );
+
+    assertEquals( component.getComputedValues().size(), 1 );
+  }
+
+  @Test
+  public void removeComputedValue_noExist()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final ComputedValue computedValue1 = context.createComputedValue( () -> "" );
+
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> component.removeComputedValue( computedValue1 ) );
+    assertEquals( exception.getMessage(), "Component.removeComputedValue invoked on component '" + name +
+                                          "' specifying computedValue named '" + computedValue1.getName() +
+                                          "' when computedValue does not exist for component." );
+  }
+
+  @Test
+  public void dispose()
+    throws Exception
+  {
+    final ArezContext context = Arez.context();
+    final String name = ValueUtil.randomString();
+
+    final Component component = new Component( context, ValueUtil.randomString(), null, name );
+
+    final Observable observable1 = context.createObservable( ValueUtil.randomString() );
+    final Observable observable2 = context.createObservable( ValueUtil.randomString() );
+    final ComputedValue computedValue1 = context.createComputedValue( () -> "" );
+    final ComputedValue computedValue2 = context.createComputedValue( () -> "" );
+    final Observer observer1 = context.autorun( () -> {
+    } );
+    final Observer observer2 = context.autorun( () -> {
+    } );
+
+    component.addObservable( observable1 );
+    component.addObservable( observable2 );
+    component.addComputedValue( computedValue1 );
+    component.addComputedValue( computedValue2 );
+    component.addObserver( observer1 );
+    component.addObserver( observer2 );
+
+    assertEquals( component.getObservables().size(), 2 );
+    assertEquals( component.getComputedValues().size(), 2 );
+    assertEquals( component.getObservers().size(), 2 );
+
+    assertFalse( Disposable.isDisposed( component ) );
+    assertFalse( Disposable.isDisposed( observable1 ) );
+    assertFalse( Disposable.isDisposed( observable2 ) );
+    assertFalse( Disposable.isDisposed( observer1 ) );
+    assertFalse( Disposable.isDisposed( observer2 ) );
+    assertFalse( Disposable.isDisposed( computedValue1 ) );
+    assertFalse( Disposable.isDisposed( computedValue2 ) );
+
+    component.dispose();
+
+    assertEquals( component.getObservables().size(), 0 );
+    assertEquals( component.getComputedValues().size(), 0 );
+    assertEquals( component.getObservers().size(), 0 );
+
+    assertTrue( Disposable.isDisposed( component ) );
+    assertTrue( Disposable.isDisposed( observable1 ) );
+    assertTrue( Disposable.isDisposed( observable2 ) );
+    assertTrue( Disposable.isDisposed( observer1 ) );
+    assertTrue( Disposable.isDisposed( observer2 ) );
+    assertTrue( Disposable.isDisposed( computedValue1 ) );
+    assertTrue( Disposable.isDisposed( computedValue2 ) );
+  }
+}
