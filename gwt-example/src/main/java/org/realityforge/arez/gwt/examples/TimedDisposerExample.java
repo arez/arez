a2599637c9babac7004351397fdf2f74e9d0954d
Peter Donald <peter@realityforge.org>
2017-12-10 15:40:10 +1100

    Fix code in `TimedDisposerExample` to avoid invariant failures when timer is disposed

--- gwt-example/src/main/java/org/realityforge/arez/gwt/examples/TimedDisposerExample.java
+++ gwt-example/src/main/java/org/realityforge/arez/gwt/examples/TimedDisposerExample.java
@@ -4,6 +4,7 @@ import com.google.gwt.core.client.EntryPoint;
 import elemental2.dom.DomGlobal;
 import org.realityforge.arez.Arez;
 import org.realityforge.arez.Disposable;
+import org.realityforge.arez.Observer;
 import org.realityforge.arez.browser.extras.IntervalTicker;
 import org.realityforge.arez.browser.extras.TimedDisposer;
 
@@ -14,7 +15,17 @@ public class TimedDisposerExample
   public void onModuleLoad()
   {
     final IntervalTicker ticker = IntervalTicker.create( 1000 );
-    Arez.context().autorun( () -> DomGlobal.console.log( "Tick: " + ticker.getTickTime() ) );
+    final Observer observer = Arez.context().autorun( () -> {
+      if ( !Disposable.isDisposed( ticker ) )
+      {
+        DomGlobal.console.log( "Tick: " + ticker.getTickTime() );
+      }
+      else
+      {
+        DomGlobal.console.log( "Ticker disposed!" );
+      }
+    } );
     TimedDisposer.create( Disposable.asDisposable( ticker ), 5500 );
+    TimedDisposer.create( Disposable.asDisposable( observer ), 7000 );
   }
 }
