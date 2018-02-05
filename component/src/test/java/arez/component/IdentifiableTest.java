ff37b986536058748bf9534afd4e729d0c28074d
Peter Donald <peter@realityforge.org>
2018-02-06 08:52:16 +1100

    Add minimal tests to Identifiable

---
+++ component/src/test/java/arez/component/IdentifiableTest.java
@@ -0,0 +1,39 @@
+package arez.component;
+
+import javax.annotation.Nonnull;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class IdentifiableTest
+  extends AbstractArezComponentTest
+{
+  static class TestElement
+    implements Identifiable<String>
+  {
+    @Nonnull
+    @Override
+    public String getArezId()
+    {
+      return "X";
+    }
+  }
+
+  @Test
+  public void identifiableElement()
+  {
+    final TestElement element = new TestElement();
+    assertEquals( Identifiable.getArezId( element ), "X" );
+    assertEquals( Identifiable.<String>asIdentifiable( element ), element );
+  }
+
+  @Test
+  public void nonIdentifiableElement()
+  {
+    final Object element = new Object();
+    assertNull( Identifiable.getArezId( element ) );
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, () -> Identifiable.asIdentifiable( element ) );
+    assertEquals( exception.getMessage(),
+                  "Object passed to asIdentifiable does not implement Identifiable. Object: " + element );
+  }
+}
