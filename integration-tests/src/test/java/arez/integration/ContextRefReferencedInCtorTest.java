a13a35f8f4a0787f8cde34b1ee7d3a765a4767e2
Peter Donald <peter@realityforge.org>
2018-02-16 13:59:41 +1100

    Add integration test to verify that invariant is correctly generated

---
+++ integration-tests/src/test/java/arez/integration/ContextRefReferencedInCtorTest.java
@@ -0,0 +1,32 @@
+package arez.integration;
+
+import arez.ArezContext;
+import arez.annotations.ArezComponent;
+import arez.annotations.ContextRef;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ContextRefReferencedInCtorTest
+  extends AbstractIntegrationTest
+{
+  @ArezComponent( allowEmpty = true )
+  static abstract class TestComponent
+  {
+    TestComponent()
+    {
+      getContext();
+    }
+
+    @ContextRef
+    abstract ArezContext getContext();
+  }
+
+  @Test
+  public void scenario()
+    throws Throwable
+  {
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, ContextRefReferencedInCtorTest_Arez_TestComponent::new );
+    assertEquals( exception.getMessage(), "Method invoked on uninitialized component of type 'TestComponent'" );
+  }
+}
