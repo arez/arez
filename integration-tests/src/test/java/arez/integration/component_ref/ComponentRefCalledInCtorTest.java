2997c34ddfcac0185724b082bbbc0c445192bdcd
Peter Donald <peter@realityforge.org>
2018-02-16 14:08:48 +1100

    Add integration tests to check behaviour of invariants in generated classes

---
+++ integration-tests/src/test/java/arez/integration/component_ref/ComponentRefCalledInCtorTest.java
@@ -0,0 +1,33 @@
+package arez.integration.component_ref;
+
+import arez.Component;
+import arez.annotations.ArezComponent;
+import arez.annotations.ComponentRef;
+import arez.integration.AbstractIntegrationTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ComponentRefCalledInCtorTest
+  extends AbstractIntegrationTest
+{
+  @ArezComponent( allowEmpty = true )
+  static abstract class TestComponent
+  {
+    TestComponent()
+    {
+      getComponent();
+    }
+
+    @ComponentRef
+    abstract Component getComponent();
+  }
+
+  @Test
+  public void scenario()
+    throws Throwable
+  {
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, ComponentRefCalledInCtorTest_Arez_TestComponent::new );
+    assertEquals( exception.getMessage(), "Method invoked on uninitialized component of type 'TestComponent'" );
+  }
+}
---
+++ integration-tests/src/test/java/arez/integration/component_ref/ComponentRefCalledInPostConstructTest.java
@@ -0,0 +1,35 @@
+package arez.integration.component_ref;
+
+import arez.Component;
+import arez.annotations.ArezComponent;
+import arez.annotations.ComponentRef;
+import arez.annotations.PostConstruct;
+import arez.integration.AbstractIntegrationTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ComponentRefCalledInPostConstructTest
+  extends AbstractIntegrationTest
+{
+  @ArezComponent( allowEmpty = true )
+  static abstract class TestComponent
+  {
+    @PostConstruct
+    void postConstruct()
+    {
+      getComponent();
+    }
+
+    @ComponentRef
+    abstract Component getComponent();
+  }
+
+  @Test
+  public void scenario()
+    throws Throwable
+  {
+    final IllegalStateException exception =
+      expectThrows( IllegalStateException.class, ComponentRefCalledInPostConstructTest_Arez_TestComponent::new );
+    assertEquals( exception.getMessage(), "Method invoked on un-constructed component named 'TestComponent.0'" );
+  }
+}
