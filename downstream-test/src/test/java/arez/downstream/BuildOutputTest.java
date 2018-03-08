be532629b1e5f6f70debf62b226f08481f2b0984
Peter Donald <peter@realityforge.org>
2018-03-09 10:44:01 +1100

    Add test that verifies that no unexpected symbols appear

---
+++ downstream-test/src/test/java/arez/downstream/BuildOutputTest.java
@@ -0,0 +1,61 @@
+package arez.downstream;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.annotation.Nonnull;
+import org.testng.annotations.Test;
+
+public class BuildOutputTest
+  extends AbstractDownstreamTest
+{
+  @Test
+  public void arezProduction()
+    throws Exception
+  {
+    final String build = "arez.after";
+
+    final SymbolEntryIndex index = SymbolEntryIndex.readSymbolMapIntoIndex( getSymbolMapPath( build ) );
+
+    // Assert no Spy cruft is enabled as !Arez.areSpiesEnabled() in the build
+    index.assertNoClassNameMatches( "arez\\.spy\\..*" );
+    index.assertNoClassNameMatches( "arez\\.Spy.*" );
+    index.assertNoClassNameMatches( "arez\\..*InfoImpl" );
+
+    // Assert no Zone cruft is enabled as !Arez.areZonesEnabled() in the build
+    index.assertNoClassNameMatches( "arez\\.Zone" );
+    index.assertNoMemberMatches( ".*\\.Arez_.*", "$$arezi$$_context" );
+
+    // Assert no Component cruft is enabled as !Arez.areNativeComponentsEnabled() in the build
+    index.assertNoClassNameMatches( "arez\\.Component.*" );
+    index.assertNoMemberMatches( ".*\\.Arez_.*", "$$arezi$$_component" );
+
+    // Assert no Transaction validation cruft is enabled as !Arez.shouldEnforceTransactionType() in the build
+    index.assertNoClassNameMatches( "arez\\.TransactionMode" );
+
+    // !Arez.areNamesEnabled in the build
+    index.assertNoClassNameMatches( "arez\\.ThrowableUtil" );
+
+    // This should never appear as it is not meant to be GWT compiled
+    index.assertNoClassNameMatches( "arez\\.ArezTestUtil" );
+
+    // This pattern should apply if nameIncludeId is false (or @Singleton present), no @Repository annotation and !Arez.areNativeComponentsEnabled()
+    index.assertNoMemberMatches( "arez\\.browser\\.extras\\.Arez_BrowserLocation", "$$arezi$$_id" );
+  }
+
+  @Nonnull
+  private Path getSymbolMapPath( @Nonnull final String build )
+    throws IOException
+  {
+    final Path symbolMapsDir =
+      getArchiveDir()
+        .resolve( build )
+        .resolve( "assets" )
+        .resolve( "WEB-INF" )
+        .resolve( "deploy" )
+        .resolve( "todomvc" )
+        .resolve( "symbolMaps" );
+
+    return Files.list( symbolMapsDir ).findFirst().orElseThrow( AssertionError::new );
+  }
+}
