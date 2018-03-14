5ef474fcc2fac5e07fc4b7e2bbb9d795542202d3
Peter Donald <peter@realityforge.org>
2018-03-14 15:59:01 +1100

    Extract out assertions that verify the output state of arez builds

--- downstream-test/src/test/java/arez/downstream/BuildOutputTest.java
+++ downstream-test/src/test/java/arez/downstream/BuildOutputTest.java
@@ -1,6 +1,7 @@
 package arez.downstream;
 
 import arez.downstream.util.SymbolEntryIndex;
+import arez.downstream.util.arez.ArezBuildAsserts;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,51 +19,10 @@ public class BuildOutputTest
 
     final SymbolEntryIndex index = SymbolEntryIndex.readSymbolMapIntoIndex( getSymbolMapPath( build ) );
 
-    // Assert no Spy cruft is enabled as !Arez.areSpiesEnabled() in the build
-    index.assertNoClassNameMatches( "arez\\.spy\\..*" );
-    index.assertNoClassNameMatches( "arez\\.Spy.*" );
-    index.assertNoClassNameMatches( "arez\\..*InfoImpl" );
-
-    // Assert no Zone cruft is enabled as !Arez.areZonesEnabled() in the build
-    index.assertNoClassNameMatches( "arez\\.Zone" );
-    index.assertNoClassNameMatches( "arez\\.ArezZoneHolder" );
-    index.assertNoMemberMatches( "arez\\.Arez", "createZone" );
-    index.assertNoMemberMatches( "arez\\.Arez", "activateZone" );
-    index.assertNoMemberMatches( "arez\\.Arez", "deactivateZone" );
-    index.assertNoMemberMatches( "arez\\.Arez", "currentZone" );
-    index.assertNoMemberMatches( ".*\\.Arez_.*", "$$arezi$$_context" );
-    index.assertNoMemberMatches( "arez\\.Node", "_context" );
-
-    // Assert no Component cruft is enabled as !Arez.areNativeComponentsEnabled() in the build
-    index.assertNoClassNameMatches( "arez\\.Component.*" );
-    index.assertNoMemberMatches( ".*\\.Arez_.*", "$$arezi$$_component" );
-    index.assertNoMemberMatches( ".*\\.Arez_.*Repository", "component" );
-
-    // Assert no Transaction validation cruft is enabled as !Arez.shouldEnforceTransactionType() in the build
-    index.assertNoClassNameMatches( "arez\\.TransactionMode" );
-
-    // Assert RepositoryUtil is eliminated once !Arez.areRepositoryResultsModifiable() in the build
-    index.assertNoClassNameMatches( "arez\\.component\\.RepositoryUtil" );
-
-    // !Arez.areNamesEnabled in the build
-    index.assertNoClassNameMatches( "arez\\.ThrowableUtil" );
-    index.assertNoMemberMatches( ".*\\.Arez_.*Repository", "getRepositoryName" );
-    index.assertNoMemberMatches( ".*\\.Arez_.*", "toString" );
-
-    // This should never appear as it is not meant to be GWT compiled
-    index.assertNoClassNameMatches( "arez\\.ArezTestUtil" );
-    index.assertNoMemberMatches( "arez\\.ArezZoneHolder", "getDefaultZone" );
-    index.assertNoMemberMatches( "arez\\.ArezZoneHolder", "getZoneStack" );
-
-    // This should be optimized out completely
-    index.assertNoClassNameMatches( "arez\\.ArezConfig" );
-
-    // This should be eliminated as it will improve the ability for GWT compiler to dead-code-eliminate
-    index.assertNoMemberMatches( "arez\\.Arez", "$clinit" );
+    ArezBuildAsserts.assertArezOutputs( index, false, false, false, false, false, false );
 
     // This pattern should apply if nameIncludeId is false (or @Singleton present), no @Repository annotation and !Arez.areNativeComponentsEnabled()
-    index.assertNoMemberMatches( "arez\\.browser\\.extras\\.Arez_BrowserLocation", "$$arezi$$_id" );
-    index.assertNoMemberMatches( "arez\\.browser\\.extras\\.Arez_BrowserLocation", "$$arezi$$_nextId" );
+    ArezBuildAsserts.assertSyntheticId( index, "arez\\.browser\\.extras\\.Arez_BrowserLocation", false );
 
     //TODO: Add tests when ArezComponent.requireEquals = DISABLE to ensure no hashCode or equals
     //TODO: assert no Observable.preReportChanged if assertions disabled
---
+++ downstream-test/src/test/java/arez/downstream/util/arez/ArezBuildAsserts.java
@@ -0,0 +1,107 @@
+package arez.downstream.util.arez;
+
+import arez.downstream.util.SymbolEntryIndex;
+import javax.annotation.Nonnull;
+import org.intellij.lang.annotations.RegExp;
+
+public final class ArezBuildAsserts
+{
+  private ArezBuildAsserts()
+  {
+  }
+
+  public static void assertStandardOutputs( @Nonnull final SymbolEntryIndex index )
+  {
+    // This should never appear as it is not meant to be GWT compiled
+    index.assertNoClassNameMatches( "arez\\.ArezTestUtil" );
+    index.assertNoMemberMatches( "arez\\.ArezZoneHolder", "getDefaultZone" );
+    index.assertNoMemberMatches( "arez\\.ArezZoneHolder", "getZoneStack" );
+
+    // This should be optimized out completely
+    index.assertNoClassNameMatches( "arez\\.ArezConfig" );
+
+    // This should be eliminated as it will improve the ability for GWT compiler to dead-code-eliminate
+    index.assertNoMemberMatches( "arez\\.Arez", "$clinit" );
+  }
+
+  public static void assertShouldEnforceTransactionTypeOutputs( @Nonnull final SymbolEntryIndex index,
+                                                                final boolean enabled )
+  {
+    // Assert no Transaction validation cruft is enabled as !Arez.shouldEnforceTransactionType() in the build
+    index.assertSymbol( "arez\\.TransactionMode", enabled );
+  }
+
+  public static void assertAreRepositoryResultsModifiableOutputs( @Nonnull final SymbolEntryIndex index,
+                                                                  final boolean enabled )
+  {
+    // Assert RepositoryUtil is eliminated once !Arez.areRepositoryResultsModifiable() in the build
+    index.assertSymbol( "arez\\.component\\.RepositoryUtil", enabled );
+  }
+
+  public static void assertAreNamesEnabled( @Nonnull final SymbolEntryIndex index,
+                                            final boolean enabled )
+  {
+    // !Arez.areNamesEnabled in the build
+    index.assertSymbol( "arez\\.ThrowableUtil", enabled );
+    index.assertSymbol( ".*\\.Arez_.*Repository", "getRepositoryName", enabled );
+    index.assertSymbol( ".*\\.Arez_.*", "toString", enabled );
+  }
+
+  public static void assertSpyOutputs( @Nonnull final SymbolEntryIndex index, final boolean enabled )
+  {
+    // Assert Spy files are either present or not present based on spiesEnabled parameter
+    index.assertSymbol( "arez\\.spy\\..*", enabled );
+    index.assertSymbol( "arez\\.Spy.*", enabled );
+    index.assertSymbol( "arez\\..*InfoImpl", enabled );
+  }
+
+  public static void assertZoneOutputs( @Nonnull final SymbolEntryIndex index, final boolean enabled )
+  {
+
+    index.assertSymbol( "arez\\.Zone", enabled );
+    index.assertSymbol( "arez\\.ArezZoneHolder", enabled );
+    index.assertSymbol( "arez\\.Arez", "createZone", enabled );
+    index.assertSymbol( "arez\\.Arez", "activateZone", enabled );
+    index.assertSymbol( "arez\\.Arez", "deactivateZone", enabled );
+    index.assertSymbol( "arez\\.Arez", "currentZone", enabled );
+    index.assertSymbol( ".*\\.Arez_.*", "$$arezi$$_context", enabled );
+    index.assertSymbol( "arez\\.Node", "_context", enabled );
+  }
+
+  public static void assertNativeComponentOutputs( @Nonnull final SymbolEntryIndex index, final boolean enabled )
+  {
+    // Assert no Component cruft is enabled as !Arez.areNativeComponentsEnabled() in the build
+    index.assertSymbol( "arez\\.Component.*", enabled );
+    index.assertSymbol( ".*\\.Arez_.*", "$$arezi$$_component", enabled );
+    index.assertSymbol( ".*\\.Arez_.*Repository", "component", enabled );
+
+    // No repositories need their own identity if native components disabled
+    assertSyntheticId( index, ".*\\.Arez_[^\\.]+Repository", false );
+  }
+
+  public static void assertSyntheticId( @Nonnull final SymbolEntryIndex index,
+                                        @RegExp( prefix = "^", suffix = "$" ) @Nonnull final String classNamePattern,
+                                        final boolean enabled )
+  {
+    index.assertSymbol( classNamePattern, "$$arezi$$_id", enabled );
+    index.assertSymbol( classNamePattern, "$$arezi$$_nextId", enabled );
+
+  }
+
+  public static void assertArezOutputs( @Nonnull final SymbolEntryIndex index,
+                                        final boolean areNamesEnabled,
+                                        final boolean areSpiesEnabled,
+                                        final boolean areNativeComponentsEnabled,
+                                        final boolean areZonesEnabled,
+                                        final boolean shouldEnforceTransactionType,
+                                        final boolean areRepositoryResultsModifiable )
+  {
+    assertStandardOutputs( index );
+    assertAreNamesEnabled( index, areNamesEnabled );
+    assertSpyOutputs( index, areSpiesEnabled );
+    assertNativeComponentOutputs( index, areNativeComponentsEnabled );
+    assertZoneOutputs( index, areZonesEnabled );
+    assertShouldEnforceTransactionTypeOutputs( index, shouldEnforceTransactionType );
+    assertAreRepositoryResultsModifiableOutputs( index, areRepositoryResultsModifiable );
+  }
+}
