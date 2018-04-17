3f0dd4b8850f391af8882a5b60e6e52cad012136
Peter Donald <peter@realityforge.org>
2018-04-17 12:27:26 +1000

    Add integration test to verify that autorun can not be invoked directly

---
+++ integration-tests/src/test/java/arez/integration/autorun/AutorunNoInvokeTest.java
@@ -0,0 +1,36 @@
+package arez.integration.autorun;
+
+import arez.annotations.ArezComponent;
+import arez.annotations.Autorun;
+import arez.integration.AbstractIntegrationTest;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class AutorunNoInvokeTest
+  extends AbstractIntegrationTest
+{
+  @ArezComponent
+  public static abstract class TestComponent
+  {
+    int _autorunCallCount;
+
+    @Autorun
+    void autorun()
+    {
+      _autorunCallCount++;
+    }
+  }
+
+  @Test
+  public void deferScheduleWillDelayAutorun()
+  {
+    final TestComponent component = new AutorunNoInvokeTest_Arez_TestComponent();
+
+    assertEquals( component._autorunCallCount, 1 );
+
+    final IllegalStateException exception = expectThrows( IllegalStateException.class, component::autorun );
+    assertEquals( exception.getMessage(),
+                  "Autorun method named 'autorun' invoked but @Autorun annotated methods should only be invoked by the runtime." );
+    assertEquals( component._autorunCallCount, 1 );
+  }
+}
