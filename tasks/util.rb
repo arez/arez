1b15665b875b576765d52ceb554fec69841e720a
Peter Donald <peter@realityforge.org>
2018-04-21 19:52:14 +1000

    Extract the list of downstream projects into a constant

--- tasks/util.rb
+++ tasks/util.rb
@@ -1,5 +1,7 @@
 WORKSPACE_DIR = File.expand_path(File.dirname(__FILE__) + '/..')
 
+DOWNSTREAM_PROJECTS=%w(arez-browserlocation arez-idlestatus arez-networkstatus arez-promise arez-spytools arez-ticker arez-timeddisposer)
+
 def in_dir(dir)
   current = Dir.pwd
   begin
