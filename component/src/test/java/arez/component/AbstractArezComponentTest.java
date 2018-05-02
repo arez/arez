28eb21b274d752f286ea88b8355e66a2b67be713
Peter Donald <peter@realityforge.org>
2018-05-02 21:12:10 +1000

    Enhance base test to pick up ObserverErrors and fail test if they appear unexpectantly

--- component/src/test/java/arez/component/AbstractArezComponentTest.java
+++ component/src/test/java/arez/component/AbstractArezComponentTest.java
@@ -1,19 +1,36 @@
 package arez.component;
 
+import arez.Arez;
 import arez.ArezTestUtil;
+import arez.Observer;
+import arez.ObserverError;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import org.realityforge.braincheck.BrainCheckTestUtil;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import static org.testng.Assert.*;
 
 @SuppressWarnings( "Duplicates" )
 public abstract class AbstractArezComponentTest
 {
+  private final ArrayList<String> _observerErrors = new ArrayList<>();
+  private boolean _ignoreObserverErrors;
+  private boolean _printObserverErrors;
+
   @BeforeMethod
   protected void beforeTest()
     throws Exception
   {
     BrainCheckTestUtil.resetConfig( false );
     ArezTestUtil.resetConfig( false );
+    ArezTestUtil.enableZones();
+    _ignoreObserverErrors = false;
+    _printObserverErrors = true;
+    _observerErrors.clear();
+    Arez.context().addObserverErrorHandler( this::onObserverError );
   }
 
   @AfterMethod
@@ -22,5 +39,37 @@ public abstract class AbstractArezComponentTest
   {
     BrainCheckTestUtil.resetConfig( true );
     ArezTestUtil.resetConfig( true );
+    if ( !_ignoreObserverErrors && !_observerErrors.isEmpty() )
+    {
+      fail( "Unexpected Observer Errors: " + _observerErrors.stream().collect( Collectors.joining( "\n" ) ) );
+    }
+  }
+
+  protected final void setIgnoreObserverErrors( final boolean ignoreObserverErrors )
+  {
+    _ignoreObserverErrors = ignoreObserverErrors;
+  }
+
+  protected final void setPrintObserverErrors( final boolean printObserverErrors )
+  {
+    _printObserverErrors = printObserverErrors;
+  }
+
+  private void onObserverError( @Nonnull final Observer observer,
+                                @Nonnull final ObserverError error,
+                                @Nullable final Throwable throwable )
+  {
+    final String message = "Observer: " + observer.getName() + " Error: " + error + " " + throwable;
+    _observerErrors.add( message );
+    if ( _printObserverErrors )
+    {
+      System.out.println( message );
+    }
+  }
+
+  @Nonnull
+  final ArrayList<String> getObserverErrors()
+  {
+    return _observerErrors;
   }
 }
