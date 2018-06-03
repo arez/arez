160ae2ecc3e83a918de1e3d8f379bc7c404d6b12
Peter Donald <peter@realityforge.org>
2018-06-03 22:21:30 +1000

    Narrow types that can be passed to the constructor so can more effectively control contents of exception

--- processor/src/main/java/arez/processor/ArezProcessorException.java
+++ processor/src/main/java/arez/processor/ArezProcessorException.java
@@ -1,8 +1,12 @@
 package arez.processor;
 
 import java.util.Objects;
+import java.util.Set;
 import javax.annotation.Nonnull;
+import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
 
 class ArezProcessorException
   extends RuntimeException
@@ -10,7 +14,22 @@ class ArezProcessorException
   @Nonnull
   private final Element _element;
 
-  ArezProcessorException( @Nonnull final String message, @Nonnull final Element element )
+  ArezProcessorException( @Nonnull final String message, @Nonnull final TypeElement element )
+  {
+    this( message, (Element) element );
+  }
+
+  ArezProcessorException( @Nonnull final String message, @Nonnull final ExecutableElement element )
+  {
+    this( message, (Element) element );
+  }
+
+  /**
+   * Private constructor to restrict the types that can be passed into exception. If any other types
+   * are passed into this constructor then the error handling in {@link ArezProcessor#processElements(Set, RoundEnvironment)}
+   * needs to be updated.
+   */
+  private ArezProcessorException( @Nonnull final String message, @Nonnull final Element element )
   {
     super( message );
     _element = Objects.requireNonNull( element );
