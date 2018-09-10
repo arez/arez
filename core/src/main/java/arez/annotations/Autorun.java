69446b2df3263777cc2745f79e6766d98454afd3
Peter Donald <peter@realityforge.org>
2018-09-10 10:25:01 +1000

    Improve the language in javadocs

--- core/src/main/java/arez/annotations/Autorun.java
+++ core/src/main/java/arez/annotations/Autorun.java
@@ -24,38 +24,38 @@ import javax.annotation.Nonnull;
 public @interface Autorun
 {
   /**
-   * Return the name of the Autorun relative to the component.
+   * Return the name of the Observer relative to the component.
    * The value must conform to the requirements of a java identifier.
    * The name must also be unique across {@link Observable}s,
    * {@link Computed}s and {@link Action}s within the scope of the
    * {@link ArezComponent} annotated element.
    *
-   * @return the name of the Autorun relative to the component.
+   * @return the name of the Observer relative to the component.
    */
   @Nonnull
   String name() default "<default>";
 
   /**
-   * Does the autorun mutate state or not.
-   * The default value us false thus making the transaction mode read-only. The intention is that autorun observers
-   * should primarily be reflecting Arez state to external systems. (i.e. views, network layers etc.)
+   * Does the observer's tracking method change arez state or not.
+   * Observers are primarily used to reflect Arez state onto external systems (i.e. views, network layers etc.)
+   * and thus the default value is false thus making the transaction mode read-only.
    *
-   * @return true if the autorun should run in a READ_WRITE transaction, false if it should run in a READ_ONLY transaction.
+   * @return true if the observer should use a read-write transaction, false if it should use a read-only transaction.
    */
   boolean mutation() default false;
 
   /**
-   * The priority of the underlying autorun observer
+   * The priority of the underlying observer
    *
-   * @return the priority of the autorun observer.
+   * @return the priority of the observer.
    */
   Priority priority() default Priority.NORMAL;
 
   /**
-   * Flag controlling whether the underlying observer can observe ComputedValue instances with lower priorities.
+   * Flag controlling whether the observer can observe ComputedValue instances with lower priorities.
    * The default value of false will result in an invariant failure (in development mode) if a lower priority
    * dependency is observed by the observer. This is to prevent priority inversion when scheduling a higher
-   * priority observer is dependent upon a lower priority computed value. If the value is true then the no
+   * priority observer that is dependent upon a lower priority computed value. If the value is true then the no
    * invariant failure is triggered and the component relies on the component author to handle possible priority
    * inversion.
    *
@@ -64,7 +64,7 @@ public @interface Autorun
   boolean observeLowerPriorityDependencies() default false;
 
   /**
-   * Flag controlling whether the observer can invoke actions.
+   * Can the observer invoke actions.
    * An action that specifies {@link Action#requireNewTransaction()} as true will start a new transaction
    * and any observables accessed within the action will not be dependencies of the observer. Sometimes this
    * behaviour is desired. Sometimes an action that specifies {@link Action#requireNewTransaction()} as false
