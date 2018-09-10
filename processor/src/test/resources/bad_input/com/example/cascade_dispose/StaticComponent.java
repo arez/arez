cc1a91f820dd6d1a5647203dd88863a611f6a551
Peter Donald <peter@realityforge.org>
2018-09-10 11:41:19 +1000

    Optimize imports

--- processor/src/test/resources/bad_input/com/example/cascade_dispose/StaticComponent.java
+++ processor/src/test/resources/bad_input/com/example/cascade_dispose/StaticComponent.java
@@ -3,7 +3,6 @@ package com.example.cascade_dispose;
 import arez.Disposable;
 import arez.annotations.ArezComponent;
 import arez.annotations.CascadeDispose;
-import java.util.List;
 
 @ArezComponent
 public abstract class StaticComponent
