b9c95066bbb14b1087b51ff8dc16d2f2958f2002
Peter Donald <peter@realityforge.org>
2018-10-08 14:25:46 +1100

    Suppress warning

--- integration-tests/src/test/java/arez/integration/action/RequiresEnvironmentTest.java
+++ integration-tests/src/test/java/arez/integration/action/RequiresEnvironmentTest.java
@@ -14,7 +14,7 @@ import javax.annotation.Nonnull;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
-@SuppressWarnings( { "UnusedReturnValue", "unused", "SameParameterValue" } )
+@SuppressWarnings( { "UnusedReturnValue", "unused", "SameParameterValue", "Duplicates" } )
 public class RequiresEnvironmentTest
   extends AbstractArezIntegrationTest
 {
