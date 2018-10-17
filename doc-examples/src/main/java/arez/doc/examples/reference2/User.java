96600451fc85b76bbdff4d6b41aca3c9f99cf039
Peter Donald <peter@realityforge.org>
2018-10-18 08:33:35 +1100

    Add code required for documentation

---
+++ doc-examples/src/main/java/arez/doc/examples/reference2/User.java
@@ -0,0 +1,52 @@
+package arez.doc.examples.reference2;
+
+import arez.annotations.ArezComponent;
+import arez.annotations.ComponentId;
+import arez.annotations.Observable;
+import arez.annotations.Repository;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+
+@ArezComponent
+@Repository
+public abstract class User
+{
+  private final int _id;
+  @Nonnull
+  private String _name;
+  private boolean _active;
+
+  public User( final int id, @Nonnull final String name )
+  {
+    _id = id;
+    _name = Objects.requireNonNull( name );
+  }
+
+  @ComponentId
+  public final int getId()
+  {
+    return _id;
+  }
+
+  @Observable
+  @Nonnull
+  public String getName()
+  {
+    return _name;
+  }
+
+  public void setName( @Nonnull final String name )
+  {
+    _name = name;
+  }
+
+  public boolean isActive()
+  {
+    return _active;
+  }
+
+  public void setActive( final boolean active )
+  {
+    _active = active;
+  }
+}
