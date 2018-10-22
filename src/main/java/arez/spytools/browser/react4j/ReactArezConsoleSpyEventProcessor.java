e22e921869525cc132b29bccfa0db37245273456
Peter Donald <peter@realityforge.org>
2018-10-22 21:25:55 +1100

    Fuse steps in stream definition

--- src/main/java/arez/spytools/browser/react4j/ReactArezConsoleSpyEventProcessor.java
+++ src/main/java/arez/spytools/browser/react4j/ReactArezConsoleSpyEventProcessor.java
@@ -3,7 +3,6 @@ package arez.spytools.browser.react4j;
 import arez.spytools.browser.ConsoleSpyEventProcessor;
 import arez.spytools.browser.StringifyReplacer;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
 /**
@@ -27,9 +26,8 @@ public class ReactArezConsoleSpyEventProcessor
       protected String[] getPropertyNames( @Nonnull final Object object )
       {
         return Arrays.stream( super.getPropertyNames( object ) ).
-          filter( n -> !n.equals( "key" ) && !n.equals( "ref" ) ).
-          collect( Collectors.toList() ).
-          toArray( new String[ 0 ] );
+          filter( n -> !n.equals( "key" ) && !n.equals( "ref" ) )
+          .toArray( String[]::new );
       }
     };
   }
