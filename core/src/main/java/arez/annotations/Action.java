5f2d1d44763d6ed3164e3fc508460719713953eb
Peter Donald <peter@realityforge.org>
2018-10-25 07:58:13 +1100

    Spelling

--- core/src/main/java/arez/annotations/Action.java
+++ core/src/main/java/arez/annotations/Action.java
@@ -49,7 +49,7 @@ public @interface Action
   boolean reportParameters() default true;
 
   /**
-   * Return true if the return tvalue of the action (if any) should be reported to the Arez spy subsystem.
+   * Return true if the return value of the action (if any) should be reported to the Arez spy subsystem.
    * It is useful to disable reporting for large, circular or just uninteresting parameters to the spy infrastructure.
    *
    * @return true to report the parameters, false otherwise.
