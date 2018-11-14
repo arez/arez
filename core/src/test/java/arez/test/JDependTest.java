fe5a8692e9b041907f2b75ff14c50d43b1fe95c5
Peter Donald <peter@realityforge.org>
2018-11-15 09:38:01 +1100

    Add new requirement

--- core/src/test/java/arez/test/JDependTest.java
+++ core/src/test/java/arez/test/JDependTest.java
@@ -44,6 +44,7 @@ public class JDependTest
 
     component.dependsUpon( braincheck );
     component.dependsUpon( arez );
+    component.dependsUpon( spy );
 
     final DependencyConstraint.MatchResult result = jdepend.analyzeDependencies( constraint );
 
