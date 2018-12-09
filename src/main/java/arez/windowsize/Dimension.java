120b0c955c1a1416d7f24b39529eb2e061a8eb51
Peter Donald <peter@realityforge.org>
2018-12-09 22:45:55 +1100

    Source code for the maing functionality of this package

---
+++ src/main/java/arez/windowsize/Dimension.java
@@ -0,0 +1,37 @@
+package arez.windowsize;
+
+public final class Dimension
+{
+  private final int _width;
+  private final int _height;
+
+  public Dimension( final int width, final int height )
+  {
+    _width = width;
+    _height = height;
+  }
+
+  public int getWidth()
+  {
+    return _width;
+  }
+
+  public int getHeight()
+  {
+    return _height;
+  }
+
+  @Override
+  public boolean equals( final Object o )
+  {
+    if ( o instanceof Dimension )
+    {
+      final Dimension other = (Dimension) o;
+      return other._width == _width && other._height == _height;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
---
+++ src/main/java/arez/windowsize/WindowSize.java
@@ -0,0 +1,97 @@
+package arez.windowsize;
+
+import elemental2.dom.Window;
+import javax.annotation.Nonnull;
+
+/**
+ * Factory for getting observable models that sizing of windows.
+ *
+ * <h1>A very simple example</h1>
+ * <pre>{@code
+ * import arez.Arez;
+ * import arez.windowsize.EventDrivenValue;
+ * import arez.windowsize.WindowSize;
+ * import com.google.gwt.core.client.EntryPoint;
+ * import elemental2.dom.DomGlobal;
+ * import elemental2.dom.Window;
+ *
+ * public class WindowSizeExample
+ *   implements EntryPoint
+ * {
+ *   public void onModuleLoad()
+ *   {
+ *     final EventDrivenValue&lt;Window, Integer&gt; innerHeight = WindowSize.innerHeight( DomGlobal.window );
+ *     final EventDrivenValue&lt;Window, Integer&gt; innerWidth = WindowSize.innerWidth( DomGlobal.window );
+ *
+ *     Arez.context().observer( () -> DomGlobal.document.querySelector( "#status" ).textContent =
+ *       "Screen size: " + innerWidth.getValue() + " x " + innerHeight.getValue() );
+ *   }
+ * }
+ * }</pre>
+ */
+public final class WindowSize
+{
+  private WindowSize()
+  {
+  }
+
+  /**
+   * Create an event driven observable component for window.innerWidth and window.innerHeight wrapped in dimension object.
+   *
+   * @param window the window.
+   * @return the event driven observable component.
+   */
+  @Nonnull
+  public static EventDrivenValue<Window, Dimension> inner( @Nonnull final Window window )
+  {
+    return EventDrivenValue.create( window, "resize", w -> new Dimension( w.innerWidth, w.innerHeight ) );
+  }
+
+  /**
+   * Create an event driven observable component for window.innerHeight.
+   *
+   * @param window the window.
+   * @return the event driven observable component.
+   */
+  @Nonnull
+  public static EventDrivenValue<Window, Integer> innerHeight( @Nonnull final Window window )
+  {
+    return EventDrivenValue.create( window, "resize", w -> w.innerHeight );
+  }
+
+  /**
+   * Create an event driven observable component for window.innerWidth.
+   *
+   * @param window the window.
+   * @return the event driven observable component.
+   */
+  @Nonnull
+  public static EventDrivenValue<Window, Integer> innerWidth( @Nonnull final Window window )
+  {
+    return EventDrivenValue.create( window, "resize", w -> w.innerWidth );
+  }
+
+  /**
+   * Create an event driven observable component for window.outerHeight.
+   *
+   * @param window the window.
+   * @return the event driven observable component.
+   */
+  @Nonnull
+  public static EventDrivenValue<Window, Integer> outerHeight( @Nonnull final Window window )
+  {
+    return EventDrivenValue.create( window, "resize", w -> w.outerHeight );
+  }
+
+  /**
+   * Create an event driven observable component for window.outerWidth.
+   *
+   * @param window the window.
+   * @return the event driven observable component.
+   */
+  @Nonnull
+  public static EventDrivenValue<Window, Integer> outerWidth( @Nonnull final Window window )
+  {
+    return EventDrivenValue.create( window, "resize", w -> w.outerWidth );
+  }
+}
