706b22a6cfad5bef3eb1f678569de0e72bda6010
Peter Donald <peter@realityforge.org>
2019-01-15 08:31:34 +1100

    Whitespace

--- core/src/main/java/arez/MultiPriorityTaskQueue.java
+++ core/src/main/java/arez/MultiPriorityTaskQueue.java
@@ -85,7 +85,7 @@ final class MultiPriorityTaskQueue
    * Add the specified task to the queue.
    * The task must not already be in the queue.
    *
-   * @param task     the task.
+   * @param task the task.
    */
   @Override
   public void queueTask( @Nonnull final Task task )
