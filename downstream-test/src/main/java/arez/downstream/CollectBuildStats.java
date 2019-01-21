b512a8c8434f11c6fd51a9f231267c654501390f
Peter Donald <peter@realityforge.org>
2019-01-21 22:27:23 +1100

    Extract some utility methods to a separate class

--- downstream-test/src/main/java/arez/downstream/CollectBuildStats.java
+++ downstream-test/src/main/java/arez/downstream/CollectBuildStats.java
@@ -2,21 +2,15 @@ package arez.downstream;
 
 import gir.Gir;
 import gir.GirException;
-import gir.delta.Patch;
 import gir.git.Git;
 import gir.io.Exec;
 import gir.io.FileUtil;
 import gir.maven.Maven;
 import gir.ruby.Buildr;
 import gir.ruby.Ruby;
-import gir.sys.SystemProperty;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Properties;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 
@@ -40,30 +34,13 @@ public final class CollectBuildStats
     throws Exception
   {
     Gir.go( () -> {
-      final boolean storeStatistics =
-        System.getProperty( "arez.deploy_test.store_statistics", "false" ).equals( "true" );
-      final String version = SystemProperty.get( "arez.next.version" );
+      final String version = WorkspaceUtil.getVersion();
       final OrderedProperties overallStatistics = new OrderedProperties();
-      final Path workingDirectory =
-        Paths.get( SystemProperty.get( "arez.deploy_test.work_dir" ) ).toAbsolutePath().normalize();
-      final Path fixtureDirectory =
-        Paths.get( SystemProperty.get( "arez.deploy_test.fixture_dir" ) ).toAbsolutePath().normalize();
+      final Path workingDirectory = WorkspaceUtil.setupWorkingDirectory();
 
-      final Path path = fixtureDirectory.resolve( "statistics.properties" );
+      final Path path = WorkspaceUtil.getFixtureDirectory().resolve( "statistics.properties" );
       final OrderedProperties fixtureStatistics = new OrderedProperties();
       fixtureStatistics.load( Files.newBufferedReader( path ) );
-
-      final String localRepositoryUrl = SystemProperty.get( "arez.deploy_test.local_repository_url" );
-
-      if ( !workingDirectory.toFile().exists() )
-      {
-        Gir.messenger().info( "Working directory does not exist." );
-        Gir.messenger().info( "Creating directory: " + workingDirectory );
-        if ( !workingDirectory.toFile().mkdirs() )
-        {
-          Gir.messenger().error( "Failed to create working directory: " + workingDirectory );
-        }
-      }
 
       FileUtil.inDirectory( workingDirectory, () -> {
         Gir.messenger().info( "Cloning react4j-todomvc into " + workingDirectory );
@@ -98,17 +75,17 @@ public final class CollectBuildStats
             {
               if ( isMaven )
               {
-                customizeMaven( appDirectory, localRepositoryUrl );
+                WorkspaceUtil.customizeMaven( appDirectory );
               }
               else
               {
-                customizeBuildr( appDirectory, localRepositoryUrl );
+                WorkspaceUtil.customizeBuildr( appDirectory );
               }
 
               final String prefix = branch + ".before";
-              final Path archiveDir = getArchiveDir( workingDirectory, prefix );
+              final Path archiveDir = WorkspaceUtil.getArchiveDir( workingDirectory, prefix );
               buildAndRecordStatistics( archiveDir, !isMaven, isj2cl );
-              loadStatistics( overallStatistics, archiveDir, prefix );
+              WorkspaceUtil.loadStatistics( overallStatistics, archiveDir, prefix );
               initialBuildSuccess = true;
             }
             catch ( final GirException | IOException e )
@@ -147,22 +124,22 @@ public final class CollectBuildStats
             Gir.messenger().info( "Building branch " + branch + " after modifications." );
             if ( isMaven )
             {
-              customizeMaven( appDirectory, localRepositoryUrl );
+              WorkspaceUtil.customizeMaven( appDirectory );
             }
             else
             {
-              customizeBuildr( appDirectory, localRepositoryUrl );
+              WorkspaceUtil.customizeBuildr( appDirectory );
             }
 
             final String prefix = branch + ".after";
-            final Path archiveDir = getArchiveDir( workingDirectory, prefix );
+            final Path archiveDir = WorkspaceUtil.getArchiveDir( workingDirectory, prefix );
             try
             {
               buildAndRecordStatistics( archiveDir, !isMaven, isj2cl );
-              loadStatistics( overallStatistics, archiveDir, prefix );
+              WorkspaceUtil.loadStatistics( overallStatistics, archiveDir, prefix );
               if ( !isMaven || isj2cl )
               {
-                loadStatistics( fixtureStatistics, archiveDir, version + "." + branch );
+                WorkspaceUtil.loadStatistics( fixtureStatistics, archiveDir, version + "." + branch );
               }
               if ( isMaven )
               {
@@ -205,72 +182,14 @@ public final class CollectBuildStats
       overallStatistics.keySet().forEach( k -> System.out.println( k + ": " + overallStatistics.get( k ) ) );
       final Path statisticsFile = workingDirectory.resolve( "statistics.properties" );
       Gir.messenger().info( "Writing overall build statistics to " + statisticsFile + "." );
-      writeProperties( statisticsFile, overallStatistics );
+      WorkspaceUtil.writeProperties( statisticsFile, overallStatistics );
 
-      if ( storeStatistics )
+      if ( WorkspaceUtil.storeStatistics() )
       {
         Gir.messenger().info( "Updating fixture build statistics at" + path + "." );
-        writeProperties( path, fixtureStatistics );
+        WorkspaceUtil.writeProperties( path, fixtureStatistics );
       }
     } );
-  }
-
-  private static void customizeBuildr( @Nonnull final Path appDirectory, @Nonnull final String localRepositoryUrl )
-  {
-    try
-    {
-      final String content = "repositories.remote.unshift('" + localRepositoryUrl + "')\n";
-      Files.write( appDirectory.resolve( "_buildr.rb" ), content.getBytes() );
-    }
-    catch ( final IOException ioe )
-    {
-      Gir.messenger().error( "Failed to emit _buildr.rb configuration file.", ioe );
-    }
-  }
-
-  private static void customizeMaven( @Nonnull final Path appDirectory, @Nonnull final String localRepositoryUrl )
-  {
-    final String replacement =
-      "<repositories>\n" +
-      "    <repository>\n" +
-      "      <id>local-repository</id>\n" +
-      "      <url>" + localRepositoryUrl + "</url>\n" +
-      "    </repository>";
-    if ( !Patch.file( appDirectory.resolve( "pom.xml" ), c -> c.replace( "<repositories>", replacement ) ) )
-    {
-      Gir.messenger().error( "Failed to patch pom.xml to add local repository." );
-    }
-  }
-
-  @Nonnull
-  private static Path getArchiveDir( @Nonnull final Path workingDirectory, @Nonnull final String build )
-  {
-    return workingDirectory.resolve( "archive" ).resolve( build );
-  }
-
-  private static void loadStatistics( @Nonnull final OrderedProperties statistics,
-                                      @Nonnull final Path archiveDir,
-                                      @Nonnull final String keyPrefix )
-    throws IOException
-  {
-    final Properties properties = new Properties();
-    properties.load( Files.newBufferedReader( archiveDir.resolve( "statistics.properties" ) ) );
-    properties.forEach( ( key, value ) -> statistics.put( keyPrefix + "." + key, value ) );
-  }
-
-  private static void writeProperties( @Nonnull final Path outputFile, @Nonnull final OrderedProperties properties )
-  {
-    try
-    {
-      properties.store( new FileWriter( outputFile.toFile() ), "" );
-      Patch.file( outputFile, c -> c.replaceAll( "#.*\n", "" ) );
-    }
-    catch ( final IOException ioe )
-    {
-      final String message = "Failed to write properties file: " + outputFile;
-      Gir.messenger().error( message, ioe );
-      throw new GirException( message, ioe );
-    }
   }
 
   private static void buildAndRecordStatistics( @Nonnull final Path archiveDir,
@@ -320,84 +239,46 @@ public final class CollectBuildStats
 
     final Path statisticsFile = archiveDir.resolve( "statistics.properties" );
     Gir.messenger().info( "Archiving statistics to " + statisticsFile + "." );
-    writeProperties( statisticsFile, properties );
+    WorkspaceUtil.writeProperties( statisticsFile, properties );
   }
 
   private static void archiveBuildrOutput( @Nonnull final Path archiveDir )
   {
     final Path currentDirectory = FileUtil.getCurrentDirectory();
-    archiveDirectory( currentDirectory.resolve( "target/assets" ), archiveDir.resolve( "assets" ) );
-    archiveDirectory( currentDirectory.resolve( "target/gwt_compile_reports/react4j.todomvc.TodomvcProd" ),
-                      archiveDir.resolve( "compileReports" ) );
+    WorkspaceUtil.archiveDirectory( currentDirectory.resolve( "target/assets" ), archiveDir.resolve( "assets" ) );
+    WorkspaceUtil.archiveDirectory( currentDirectory.resolve( "target/gwt_compile_reports/react4j.todomvc.TodomvcProd" ),
+                                    archiveDir.resolve( "compileReports" ) );
   }
 
   private static void archivej2clOutput( @Nonnull final Path archiveDir )
   {
-    archiveDirectory( FileUtil.getCurrentDirectory().resolve( "out/sources" ), archiveDir.resolve( "sources" ) );
-    archiveFile( FileUtil.getCurrentDirectory().resolve( "out/app.js" ),
-                 archiveDir.resolve( "assets/todomvc/todomvc.nocache.js" ) );
-    archiveFile( FileUtil.getCurrentDirectory().resolve( "out/app.map" ),
-                 archiveDir.resolve( "assets/todomvc/todomvc.nocache.map" ) );
+    WorkspaceUtil.archiveDirectory( FileUtil.getCurrentDirectory().resolve( "out/sources" ),
+                                    archiveDir.resolve( "sources" ) );
+    WorkspaceUtil.archiveFile( FileUtil.getCurrentDirectory().resolve( "out/app.js" ),
+                               archiveDir.resolve( "assets/todomvc/todomvc.nocache.js" ) );
+    WorkspaceUtil.archiveFile( FileUtil.getCurrentDirectory().resolve( "out/app.map" ),
+                               archiveDir.resolve( "assets/todomvc/todomvc.nocache.map" ) );
   }
 
   private static void archiveMavenOutput( @Nonnull final Path archiveDir )
   {
-    archiveDirectory( FileUtil.getCurrentDirectory().resolve( "target/react4j-todomvc-1.0.0-SNAPSHOT" ),
-                      archiveDir.resolve( "assets" ) );
-    archiveDirectory( FileUtil.getCurrentDirectory().resolve( "target/extras" ),
-                      archiveDir.resolve( "compileReports" ) );
-  }
-
-  private static void archiveFile( @Nonnull final Path sourceFilename, @Nonnull final Path targetFilename )
-  {
-    Gir.messenger().info( "Archiving output file " + sourceFilename + " to " + targetFilename );
-    try
-    {
-      final Path dir = targetFilename.getParent();
-      if ( !dir.toFile().exists() )
-      {
-        if ( !dir.toFile().mkdirs() )
-        {
-          Gir.messenger().error( "Failed to create archive directory: " + dir );
-        }
-      }
-      Files.copy( sourceFilename, targetFilename );
-    }
-    catch ( final IOException e )
-    {
-      final String message = "Failed to archive file: " + sourceFilename;
-      Gir.messenger().error( message, e );
-    }
-  }
-
-  private static void archiveDirectory( @Nonnull final Path assetsDir, @Nonnull final Path targetDir )
-  {
-    Gir.messenger().info( "Archiving output " + assetsDir + " to " + targetDir );
-    try
-    {
-      FileUtil.copyDirectory( assetsDir, targetDir );
-    }
-    catch ( final GirException e )
-    {
-      final String message = "Failed to archive directory: " + assetsDir;
-      Gir.messenger().error( message, e );
-    }
+    WorkspaceUtil.archiveDirectory( FileUtil.getCurrentDirectory().resolve( "target/react4j-todomvc-1.0.0-SNAPSHOT" ),
+                                    archiveDir.resolve( "assets" ) );
+    WorkspaceUtil.archiveDirectory( FileUtil.getCurrentDirectory().resolve( "target/extras" ),
+                                    archiveDir.resolve( "compileReports" ) );
   }
 
   private static long getTodoMvcSize( @Nonnull final Path archiveDir )
   {
-    return getFileSize( archiveDir.resolve( "assets" ).resolve( "todomvc" ).resolve( "todomvc.nocache.js" ) );
+    return WorkspaceUtil.getFileSize( archiveDir.resolve( "assets" )
+                                        .resolve( "todomvc" )
+                                        .resolve( "todomvc.nocache.js" ) );
   }
 
   private static long getTodoMvcGzSize( @Nonnull final Path archiveDir )
   {
-    return getFileSize( archiveDir.resolve( "assets" ).resolve( "todomvc" ).resolve( "todomvc.nocache.js.gz" ) );
-  }
-
-  private static long getFileSize( @Nonnull final Path path )
-  {
-    final File file = path.toFile();
-    assert file.exists();
-    return file.length();
+    return WorkspaceUtil.getFileSize( archiveDir.resolve( "assets" )
+                                        .resolve( "todomvc" )
+                                        .resolve( "todomvc.nocache.js.gz" ) );
   }
 }
---
+++ downstream-test/src/main/java/arez/downstream/WorkspaceUtil.java
@@ -0,0 +1,173 @@
+package arez.downstream;
+
+import gir.Gir;
+import gir.GirException;
+import gir.delta.Patch;
+import gir.io.FileUtil;
+import gir.sys.SystemProperty;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import javax.annotation.Nonnull;
+
+final class WorkspaceUtil
+{
+  private WorkspaceUtil()
+  {
+  }
+
+  static boolean storeStatistics()
+  {
+    return System.getProperty( "arez.deploy_test.store_statistics", "false" ).equals( "true" );
+  }
+
+  @Nonnull
+  static String getVersion()
+  {
+    return SystemProperty.get( "arez.next.version" );
+  }
+
+  @Nonnull
+  static Path getFixtureDirectory()
+  {
+    return Paths.get( SystemProperty.get( "arez.deploy_test.fixture_dir" ) ).toAbsolutePath().normalize();
+  }
+
+  @Nonnull
+  static String getLocalRepositoryUrl()
+  {
+    return SystemProperty.get( "arez.deploy_test.local_repository_url" );
+  }
+
+  @Nonnull
+  static Path setupWorkingDirectory()
+  {
+    final Path workingDirectory =
+      Paths.get( SystemProperty.get( "arez.deploy_test.work_dir" ) ).toAbsolutePath().normalize();
+    if ( !workingDirectory.toFile().exists() )
+    {
+      Gir.messenger().info( "Working directory does not exist." );
+      Gir.messenger().info( "Creating directory: " + workingDirectory );
+      if ( !workingDirectory.toFile().mkdirs() )
+      {
+        Gir.messenger().error( "Failed to create working directory: " + workingDirectory );
+      }
+    }
+    return workingDirectory;
+  }
+
+  static void archiveFile( @Nonnull final Path sourceFilename, @Nonnull final Path targetFilename )
+  {
+    Gir.messenger().info( "Archiving output file " + sourceFilename + " to " + targetFilename );
+    try
+    {
+      final Path dir = targetFilename.getParent();
+      if ( !dir.toFile().exists() )
+      {
+        if ( !dir.toFile().mkdirs() )
+        {
+          Gir.messenger().error( "Failed to create archive directory: " + dir );
+        }
+      }
+      Files.copy( sourceFilename, targetFilename );
+    }
+    catch ( final IOException e )
+    {
+      final String message = "Failed to archive file: " + sourceFilename;
+      Gir.messenger().error( message, e );
+    }
+  }
+
+  static void archiveDirectory( @Nonnull final Path assetsDir, @Nonnull final Path targetDir )
+  {
+    Gir.messenger().info( "Archiving output " + assetsDir + " to " + targetDir );
+    try
+    {
+      FileUtil.copyDirectory( assetsDir, targetDir );
+    }
+    catch ( final GirException e )
+    {
+      final String message = "Failed to archive directory: " + assetsDir;
+      Gir.messenger().error( message, e );
+    }
+  }
+
+  static long getFileSize( @Nonnull final Path path )
+  {
+    final File file = path.toFile();
+    assert file.exists();
+    return file.length();
+  }
+
+  static void writeProperties( @Nonnull final Path outputFile, @Nonnull final OrderedProperties properties )
+  {
+    try
+    {
+      properties.store( new FileWriter( outputFile.toFile() ), "" );
+      Patch.file( outputFile, c -> c.replaceAll( "#.*\n", "" ) );
+    }
+    catch ( final IOException ioe )
+    {
+      final String message = "Failed to write properties file: " + outputFile;
+      Gir.messenger().error( message, ioe );
+      throw new GirException( message, ioe );
+    }
+  }
+
+  static void loadStatistics( @Nonnull final OrderedProperties statistics,
+                              @Nonnull final Path archiveDir,
+                              @Nonnull final String keyPrefix )
+    throws IOException
+  {
+    final Properties properties = new Properties();
+    properties.load( Files.newBufferedReader( archiveDir.resolve( "statistics.properties" ) ) );
+    properties.forEach( ( key, value ) -> statistics.put( keyPrefix + "." + key, value ) );
+  }
+
+  @Nonnull
+  static Path getArchiveDir( @Nonnull final Path workingDirectory, @Nonnull final String build )
+  {
+    return workingDirectory.resolve( "archive" ).resolve( build );
+  }
+
+  static void customizeMaven( @Nonnull final Path appDirectory )
+  {
+    customizeMaven( appDirectory, getLocalRepositoryUrl() );
+  }
+
+  static void customizeMaven( @Nonnull final Path appDirectory, @Nonnull final String localRepositoryUrl )
+  {
+    final String replacement =
+      "<repositories>\n" +
+      "    <repository>\n" +
+      "      <id>local-repository</id>\n" +
+      "      <url>" + localRepositoryUrl + "</url>\n" +
+      "    </repository>";
+    if ( !Patch.file( appDirectory.resolve( "pom.xml" ), c -> c.replace( "<repositories>", replacement ) ) )
+    {
+      Gir.messenger().error( "Failed to patch pom.xml to add local repository." );
+    }
+  }
+
+  static void customizeBuildr( @Nonnull final Path appDirectory )
+  {
+    customizeBuildr( appDirectory, getLocalRepositoryUrl() );
+  }
+
+  static void customizeBuildr( @Nonnull final Path appDirectory, @Nonnull final String localRepositoryUrl )
+  {
+    try
+    {
+      final String content = "repositories.remote.unshift('" + localRepositoryUrl + "')\n";
+      Files.write( appDirectory.resolve( "_buildr.rb" ), content.getBytes() );
+    }
+    catch ( final IOException ioe )
+    {
+      Gir.messenger().error( "Failed to emit _buildr.rb configuration file.", ioe );
+    }
+  }
+}
