3fb091c47f042471c955f812d735a0579e38aa55
Peter Donald <peter@realityforge.org>
2019-01-25 13:43:32 +1100

    Add some more integration tests

---
+++ integration-tests/src/test/java/arez/integration/dagger/codegen/classification/MemoizeConsumeMethodInjectTest.java
@@ -0,0 +1,50 @@
+package arez.integration.dagger.codegen.classification;
+
+import arez.annotations.ArezComponent;
+import arez.annotations.InjectMode;
+import arez.annotations.Memoize;
+import arez.integration.dagger.codegen.AbstractCodegenIntegrationTest;
+import javax.inject.Inject;
+import org.testng.annotations.Test;
+
+public class MemoizeConsumeMethodInjectTest
+  extends AbstractCodegenIntegrationTest
+{
+  static class MyDependency
+  {
+    @Inject
+    MyDependency()
+    {
+    }
+  }
+
+  @ArezComponent( inject = InjectMode.CONSUME )
+  public static abstract class MyComponent
+  {
+    @Inject
+    void setDep( MyDependency myDependency )
+    {
+    }
+
+    @Memoize
+    int calc()
+    {
+      return 0;
+    }
+  }
+
+  @Test
+  public void scenario()
+    throws Exception
+  {
+    assertClassPresent( "Arez_MyComponent" );
+    assertClassPresent( "MyComponentDaggerModule" );
+    assertClassNotPresent( "Arez_MyComponent$Enhancer" );
+    assertClassNotPresent( "Arez_MyComponent$Factory" );
+    assertClassNotPresent( "MyComponentDaggerComponentExtension" );
+    assertClassNotPresent( "MyComponentDaggerComponentExtension$InjectSupport" );
+    assertClassNotPresent( "MyComponentDaggerComponentExtension$DaggerModule" );
+    assertClassNotPresent( "MyComponentDaggerComponentExtension$EnhancerDaggerModule" );
+    assertClassNotPresent( "MyComponentDaggerComponentExtension$DaggerSubcomponent" );
+  }
+}
