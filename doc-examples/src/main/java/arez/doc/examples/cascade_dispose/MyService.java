7d4cdb6751ebc3d23f7f000887fa941785bbfe2a
Peter Donald <peter@realityforge.org>
2019-10-09 13:54:47 +1100

    Cascade the dispose

--- doc-examples/src/main/java/arez/doc/examples/cascade_dispose/MyService.java
+++ doc-examples/src/main/java/arez/doc/examples/cascade_dispose/MyService.java
@@ -3,11 +3,13 @@ package arez.doc.examples.cascade_dispose;
 import arez.Disposable;
 import arez.annotations.Action;
 import arez.annotations.ArezComponent;
+import arez.annotations.CascadeDispose;
 import arez.annotations.PreDispose;
 
 @ArezComponent
 public abstract class MyService
 {
+  @CascadeDispose
   private final MySubService _subService = MySubService.create();
 
   @PreDispose
