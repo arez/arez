c8335e8cb69dd9abf671bfb227f49256486f4dd5
Peter Donald <peter@realityforge.org>
2019-10-16 09:03:08 +1100

    Suppress warning

--- processor/src/test/resources/bad_input/com/example/component_id/ComponentIdNotPrivateModel.java
+++ processor/src/test/resources/bad_input/com/example/component_id/ComponentIdNotPrivateModel.java
@@ -7,6 +7,7 @@ import arez.annotations.Observable;
 @ArezComponent
 public abstract class ComponentIdNotPrivateModel
 {
+  @SuppressWarnings( "FinalPrivateMethod" )
   @ComponentId
   private final long getId()
   {
