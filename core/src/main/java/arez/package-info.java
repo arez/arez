7e0e2a87784bd8f1fdeea1ef7a661f47f9a5ecfa
Peter Donald <peter@realityforge.org>
2019-10-16 23:06:30 +1100

    Verify no clinits present

--- core/src/main/java/arez/package-info.java
+++ core/src/main/java/arez/package-info.java
@@ -8,6 +8,10 @@
 // No repositories need their own identity if native components disabled
 @OmitPattern( type = "^.*(\\.|_)Arez_[^\\.]Repository$", symbol = "^\\$\\$arezi\\$\\$_id$", unless = "arez.enable_native_components" )
 @OmitPattern( type = "^.*(\\.|_)Arez_[^\\.]Repository$", symbol = "^\\$\\$arezi\\$\\$_nextId$", unless = "arez.enable_native_components" )
+@OmitPattern( symbol = "^\\$clinit$" )
+@KeepPattern( type = "^arez\\.ArezContextHolder$", symbol = "^\\$clinit$", unless = "arez.enable_zones" )
+@KeepPattern( type = "^arez\\.ArezZoneHolder$", symbol = "^\\$clinit$", when = "arez.enable_zones" )
 package arez;
 
+import grim.annotations.KeepPattern;
 import grim.annotations.OmitPattern;
