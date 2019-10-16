9117c9b55ff9993d10c9841a4eac8b95be74716f
Peter Donald <peter@realityforge.org>
2019-10-16 23:20:35 +1100

    Make sure package is never output to Javascript

--- core/src/main/java/arez/annotations/package-info.java
+++ core/src/main/java/arez/annotations/package-info.java
@@ -1,4 +1,7 @@
 /**
  * This package contains annotations used by the annotation processor to generate model elements.
  */
+@OmitPattern
 package arez.annotations;
+
+import grim.annotations.OmitPattern;
