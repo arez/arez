90c6327d50db210ecca74c3c6ea55f8d925ccf2d
Peter Donald <peter@realityforge.org>
2019-12-03 08:08:00 +1100

    Slight improvement to PostConstruct wordage

--- core/src/main/java/arez/annotations/PostDispose.java
+++ core/src/main/java/arez/annotations/PostDispose.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 /**
  * Annotation to identify method that is invoked after the component is disposed.
  * At most 1 method should be annotated with this annotation.
- * The PostDispose method is the last method invoked during dispose operation and it occurs within the scope
+ * This method is the last method invoked during the dispose operation and it occurs within the scope
  * of the transaction that dispose is occurring within.
  *
  * <p>The method that is annotated with this annotation must comply with the additional constraints:</p>
