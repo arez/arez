370aac3cd2163abc8c8c3fc61c8ab1c0e11542e6
Peter Donald <peter@realityforge.org>
2020-04-28 20:01:43 +1000

    Optimize imports

--- processor/src/main/java/arez/processor/ArezUtils.java
+++ processor/src/main/java/arez/processor/ArezUtils.java
@@ -5,11 +5,8 @@ import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
-import org.realityforge.proton.AnnotationsUtil;
 import org.realityforge.proton.MemberChecks;
 import org.realityforge.proton.ProcessorException;
 
