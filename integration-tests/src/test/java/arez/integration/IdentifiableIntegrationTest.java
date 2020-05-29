368808eee4701f78d2931970d28674c4db0428dc
Peter Donald <peter@realityforge.org>
2020-05-29 14:27:24 +1000

    Fix test to reflect current defaults

--- integration-tests/src/test/java/arez/integration/IdentifiableIntegrationTest.java
+++ integration-tests/src/test/java/arez/integration/IdentifiableIntegrationTest.java
@@ -2,6 +2,7 @@ package arez.integration;
 
 import arez.annotations.ArezComponent;
 import arez.annotations.ComponentId;
+import arez.annotations.Feature;
 import arez.component.Identifiable;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
@@ -11,7 +12,7 @@ public final class IdentifiableIntegrationTest
   extends AbstractArezIntegrationTest
 {
   @Test
-  public void arezManagedArezId()
+  public void requireId_DISABLE()
   {
     final Model1 model = new IdentifiableIntegrationTest_Arez_Model1();
 
@@ -19,7 +20,7 @@ public final class IdentifiableIntegrationTest
   }
 
   @Test
-  public void componentManagedArezId()
+  public void requireId_ENABLE()
   {
     final Model2 model = new IdentifiableIntegrationTest_Arez_Model2( 33 );
 
@@ -28,12 +29,22 @@ public final class IdentifiableIntegrationTest
     assertEquals( Identifiable.asIdentifiable( model ), model );
   }
 
-  @ArezComponent( allowEmpty = true )
+  @Test
+  public void requireId_DEFAULT()
+  {
+    final Model3 model = new IdentifiableIntegrationTest_Arez_Model3();
+
+    assertTrue( model instanceof Identifiable );
+    assertEquals( Identifiable.getArezId( model ), (Integer) 1 );
+    assertEquals( Identifiable.asIdentifiable( model ), model );
+  }
+
+  @ArezComponent( allowEmpty = true, requireId = Feature.DISABLE )
   static abstract class Model1
   {
   }
 
-  @ArezComponent( allowEmpty = true )
+  @ArezComponent( allowEmpty = true, requireId = Feature.ENABLE )
   static abstract class Model2
   {
     private final int id;
@@ -48,5 +59,10 @@ public final class IdentifiableIntegrationTest
     {
       return id;
     }
+  }
+
+  @ArezComponent( allowEmpty = true, requireId = Feature.AUTODETECT )
+  static abstract class Model3
+  {
   }
 }
