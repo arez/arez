e972442663c08c1026890f15dab38ea85f2f9d89
Peter Donald <peter@realityforge.org>
2026-01-23 14:29:10 +1100

    Add period to avoid javadoc errors

--- core/src/main/java/arez/annotations/Memoize.java
+++ core/src/main/java/arez/annotations/Memoize.java
@@ -66,7 +66,7 @@ public @interface Memoize
   boolean keepAlive() default false;
 
   /**
-   * The priority of the underlying ComputableValue observer
+   * The priority of the underlying ComputableValue observer.
    *
    * @return the priority of the ComputableValue observer.
    */
